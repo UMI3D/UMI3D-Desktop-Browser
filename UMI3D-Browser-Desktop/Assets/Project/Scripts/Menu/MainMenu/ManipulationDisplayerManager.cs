92f3f7e9fab8451db52f5e325032cf672540bf0a
Quentin-Tran <quentin.tran@gfi.fr>
2020-10-14 17:51:59 +0200

    ManipulationDisplayer with UI Tool Kit

---
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/Menu/MainMenu/ManipulationDisplayerManager.cs
@@ -0,0 +1,59 @@
+﻿/*
+Copyright 2019 Gfi Informatique
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using Unity.UIElements.Runtime;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace BrowserDesktop.Menu
+{
+    public class ManipulationDisplayerManager : umi3d.common.Singleton<ManipulationDisplayerManager>
+    {
+        public PanelRenderer panelRenderer;
+        public VisualTreeAsset manipualtionTreeAsset;
+
+        public string activeClassName;
+
+        [SerializeField]
+        private string containerTagName;
+
+        VisualElement container;
+
+        private void Start()
+        {
+            Debug.Assert(manipualtionTreeAsset != null);
+            Debug.Assert(panelRenderer != null);
+            Debug.Assert(!string.IsNullOrEmpty(activeClassName));
+            Debug.Assert(!string.IsNullOrEmpty(containerTagName));
+
+            container = panelRenderer.visualTree.Q<VisualElement>(containerTagName);
+        }
+
+        public static ManipulationElement CreateDisplayer()
+        {
+            if (Exists)
+            {
+                var manipulationElement = Instance.manipualtionTreeAsset.CloneTree().Q<ManipulationElement>();
+                Instance.container.Add(manipulationElement);
+
+                return manipulationElement;
+            }
+            return null;
+        }
+    }
+}
---
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/Menu/MainMenu/ManipulationElement.cs
@@ -0,0 +1,56 @@
+﻿/*
+Copyright 2019 Gfi Informatique
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace BrowserDesktop.Menu
+{
+    public class ManipulationElement : VisualElement
+    {
+        // UxmlFactory and UxmlTraits allow UIBuilder to use CardElement as a building block
+        public new class UxmlFactory : UxmlFactory<ManipulationElement, UxmlTraits> { }
+        public new class UxmlTraits : VisualElement.UxmlTraits { }
+
+        public void SetUp(string label, Sprite icon)
+        {
+            this.Q<Label>("name").text = label;
+            if (icon != null)
+            {
+                this.Q<VisualElement>("icon").style.backgroundImage = new StyleBackground(icon.texture);
+            }
+        }
+
+        public void SetState(bool isActive) {
+            if (isActive)
+                AddToClassList(ManipulationDisplayerManager.Instance.activeClassName);
+            else
+                RemoveFromClassList(ManipulationDisplayerManager.Instance.activeClassName);
+        }
+
+        public void Display(bool display)
+        {
+            style.display = display ? DisplayStyle.Flex : DisplayStyle.None;
+        }
+
+        public void Remove()
+        {
+            RemoveFromHierarchy();
+        }
+    }
+}
