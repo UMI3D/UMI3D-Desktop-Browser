8d9868e934394a38126103f3a204349fc6bb3599
Theo-Desbonnets <theo.desbonnets@inetum.com>
2021-10-07 11:47:51 +0200

    update sdk

--- UMI3D-Browser-Desktop/Assets/UMI3D SDK/ClientDevelopmentKit/Collaboration/Runtime/AudioVideo/AudioReader.cs
+++ UMI3D-Browser-Desktop/Assets/UMI3D SDK/ClientDevelopmentKit/Collaboration/Runtime/AudioVideo/AudioReader.cs
@@ -30,7 +30,6 @@ namespace umi3d.cdk.collaboration
         public void UpdateFrequency(int frequency)
         {
             this.frequency = frequency;
-            Debug.Log($"update frequency in reader to {frequency} {decoder != null}");
             if (decoder != null)
             {
                 OnDisable();
