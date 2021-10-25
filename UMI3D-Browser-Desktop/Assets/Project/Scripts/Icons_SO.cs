e2ba3c9f1d9642b68001bd0050c11be52010cacd
Romain Tirbisch <romain.tirbisch@inetum.com>
2021-10-25 18:24:46 +0200

    Key binding. Update: Icons SO.

--- UMI3D-Browser-Desktop/Assets/Project/Scripts/Icons_SO.cs
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/Icons_SO.cs
@@ -6,14 +6,24 @@ using UnityEngine;
 public class Icons_SO : ScriptableObject
 {
     [System.Serializable]
-    public struct Icon
+    public class Icon
     {
         [SerializeField]
-        private string key;
-        public string Key
+        [ContextMenuItem("GetKeyStringFromCode", "GetKeyStringFromCode")]
+        private string key_string;
+        public string Key_string
         {
-            get => this.key;
-            private set => this.key = value;
+            get => this.key_string;
+            private set => this.key_string = value;
+        }
+
+        [SerializeField]
+        [ContextMenuItem("GetKeyCodeFromString", "GetKeyCodeFromString")]
+        private KeyCode key_code;
+        public KeyCode Key_code
+        {
+            get => this.key_code;
+            private set => this.key_code = value;
         }
 
         [SerializeField]
@@ -24,12 +34,29 @@ public class Icons_SO : ScriptableObject
             private set => this.sprite = value;
         }
 
-        public readonly string SpriteName => sprite.name;
-
-        public Icon(string key, Sprite sprite)
+        public Icon(string key, KeyCode keyCode, Sprite sprite)
         {
-            this.key = key;
+            this.key_string = key;
+            this.key_code = keyCode;
             this.sprite = sprite;
+        }
+
+        public void GetKeyCodeFromString()
+        {
+            if (!string.IsNullOrEmpty(Key_string))
+            {
+                Debug.Log("key code test = " + key_string);
+                System.Enum.TryParse<KeyCode>(Key_string.Trim().Replace(" ", string.Empty), true, out key_code);
+            }
+        }
+
+        public void GetKeyStringFromCode()
+        {
+            if (Key_code != KeyCode.None)
+            {
+                Debug.Log("key string test = " + Key_code.ToString());
+                key_string = Key_code.ToString();
+            }
         }
     }
 
@@ -41,11 +68,39 @@ public class Icons_SO : ScriptableObject
     {
         foreach (Icon icon in icons)
         {
-            if (icon.Key == key)
+            if (icon.Key_string == key)
                 return icon.Sprite;
         }
 
         Debug.LogWarning("Icon key not found: this shouln't happen");
         return null;
+    }
+
+    public Sprite GetSpriteFrom(KeyCode key)
+    {
+        foreach (Icon icon in icons)
+        {
+            if (icon.Key_code == key)
+                return icon.Sprite;
+        }
+
+        Debug.LogWarning("Icon key not found: this shouln't happen");
+        return null;
+    }
+
+    private void GetKeyCodeFromString()
+    {
+        foreach (Icon icon in icons)
+        {
+            icon.GetKeyCodeFromString();
+        }
+    }
+
+    private void GetKeyStringFromCode()
+    {
+        foreach (Icon icon in icons)
+        {
+            icon.GetKeyStringFromCode();
+        }
     }
 }
