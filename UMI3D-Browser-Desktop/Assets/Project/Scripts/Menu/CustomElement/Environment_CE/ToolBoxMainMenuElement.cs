0ae63a1438d6c2447f0a20b0daac45508e39bf41
Romain Tirbisch <romain.tirbisch@inetum.com>
2021-10-29 17:26:29 +0200

    Add: Menu Bar class and UXML

---
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/Menu/CustomElement/Environment_CE/ToolBoxMainMenuElement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ToolBoxMainMenuElement : VisualElement
+{
+    /// <summary>
+    /// To be recognized by UI Builder
+    /// </summary>
+    public new class UxmlFactory : UxmlFactory<DropdownElement, UxmlTraits> { }
+    public new class UxmlTraits : VisualElement.UxmlTraits { }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
