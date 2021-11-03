ad9c353d8d8be928813c9c687a41d94ec991505f
Romain Tirbisch <romain.tirbisch@inetum.com>
2021-11-03 12:02:51 +0100

    Implement: menu bar buttons (toolbox, settings and leaving)

--- UMI3D-Browser-Desktop/Assets/Project/Scripts/Menu/Environment_M/MenuBar_UIController.cs
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/Menu/Environment_M/MenuBar_UIController.cs
@@ -44,15 +44,46 @@ namespace BrowserDesktop.Menu.Environment
             Debug.Assert(uiDocument != null, "uiDocument null ref in MenuBar_UIController");
             Debug.Assert(toolboxGenericElement_VTA != null, "toolboxGenericElement_VTA null ref in MenuBar_UIController");
             Debug.Assert(toolboxButtonGenericElement_VTA != null, "toolboxButtonGenericElement_VTA null ref in MenuBar_UIController");
+
+            UIBinding();
+            SetupMenuBar();
+        }
+
+        private void UIBinding()
+        {
+            var menuBar = uiDocument.rootVisualElement.Q<VisualElement>("menu-bar").Q<UI.CustomElement.MenuBarElement>();
+            openToolbox_TGE = menuBar.Q<VisualElement>("toolbox-openBtn").Q<ToolboxGenericElement>();
+            settings_TGE = menuBar.Q<VisualElement>("environment-settings").Q<ToolboxGenericElement>();
+            leaveEnvironment_TGE = menuBar.Q<VisualElement>("environment-leaveBtn").Q<ToolboxGenericElement>();
         }
 
         private void SetupMenuBar()
         {
             openToolbox_TGE.Setup("");
+            settings_TGE.Setup("");
+            leaveEnvironment_TGE.Setup("");
 
             ToolboxButtonGenericElement openToolboxButton_TBGE = toolboxButtonGenericElement_VTA.CloneTree().Q<ToolboxButtonGenericElement>();
-            //openToolboxButton_TBGE.Setup()
+            openToolboxButton_TBGE.Setup("Toolbox", "toolbox", "toolbox", true, () => { Debug.Log("TODO"); });
             openToolbox_TGE.AddTool(openToolboxButton_TBGE);
+
+            ToolboxButtonGenericElement avatar_TBGE = toolboxButtonGenericElement_VTA.CloneTree().Q<ToolboxButtonGenericElement>();
+            avatar_TBGE.Setup("", "avatarOn", "avatarOff", true, () => { Debug.Log("TODO"); });
+            //settings_TGE.AddTool(avatar_TBGE);
+
+            ToolboxButtonGenericElement sound_TBGE = toolboxButtonGenericElement_VTA.CloneTree().Q<ToolboxButtonGenericElement>();
+            sound_TBGE.Setup("", "soundOn", "soundOff", true, () => { Debug.Log("TODO"); });
+            //settings_TGE.AddTool(sound_TBGE);
+
+            ToolboxButtonGenericElement mic_TBGE = toolboxButtonGenericElement_VTA.CloneTree().Q<ToolboxButtonGenericElement>();
+            mic_TBGE.Setup("", "micOn", "micOff", true, () => { Debug.Log("TODO"); });
+            //settings_TGE.AddTool(mic_TBGE);
+
+            settings_TGE.AddTools(new ToolboxButtonGenericElement[3] { avatar_TBGE, sound_TBGE, mic_TBGE });
+
+            ToolboxButtonGenericElement leave_TBGE = toolboxButtonGenericElement_VTA.CloneTree().Q<ToolboxButtonGenericElement>();
+            leave_TBGE.Setup("", "leave", "leave", true, () => { Debug.Log("TODO"); });
+            leaveEnvironment_TGE.AddTool(leave_TBGE);
         }
 
     }
