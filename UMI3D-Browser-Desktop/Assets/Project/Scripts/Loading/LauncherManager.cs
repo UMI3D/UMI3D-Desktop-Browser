4b16c879b1c7eed0736329a8eece3d0d21a04103
Theo Desbonnets <theo.desbonnets@inetum.com>
2021-11-12 17:02:09 +0100

    Update sdk to 2.4.b.211112

--- UMI3D-Browser-Desktop/Assets/Project/Scripts/Loading/LauncherManager.cs
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/Loading/LauncherManager.cs
@@ -28,6 +28,7 @@ using BrowserDesktop.Controller;
 using BeardedManStudios.Forge.Networking;
 using System.Runtime.InteropServices;
 using System.Text;
+using umi3d.cdk.collaboration;
 
 public class LauncherManager : MonoBehaviour
 {
