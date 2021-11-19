ee3536854e979bfc65dabd986e9466389ebeee84
cemarichal <charles-eole.marichal@inetum.com>
2021-11-19 18:13:22 +0100

    Separated Filters in differents files

---
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/Selection/Intent/Scripts/Utils/LinearKalmanFilter.cs
@@ -0,0 +1,109 @@
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using System;
+
+namespace KalmanFilter
+{
+    /// <summary>
+    /// To use with linear models only
+    /// Row majors matrices are expected
+    /// </summary>
+    public class KalmanFilter : AbstractKalmanFilter
+    {
+        /// <summary>
+        /// State-transition model, also known as dynamic model
+        /// </summary>
+        private Matrix<double> Fk;
+
+        /// <summary>
+        /// Observation model
+        /// </summary>
+        private Matrix<double> Hk;
+
+        public KalmanFilter(int dimStates, int dimObservation, double stdProcess, double stdMeasure, double[][] modelProcess, double[][] modelObservation)
+            : base(dimStates, dimObservation)
+        {
+            q = stdProcess;
+            r = stdMeasure;
+
+            Q = Matrix.Build.Diagonal(L, L, q * q);
+            R = Matrix.Build.Diagonal(M, M, r * r);
+
+            Fk = Matrix.Build.DenseOfRowArrays(modelProcess);
+            Hk = Matrix.Build.DenseOfRowArrays(modelObservation);
+        }
+
+        /// <summary>
+        /// Produce a Kalman filter with observation of the same dimension than the states and with an identity relationship
+        /// </summary>
+        /// <param name="dimStates"></param>
+        /// <param name="stdProcess"></param>
+        /// <param name="stdMeasure"></param>
+        /// <param name="modelProcess"></param>
+        public KalmanFilter(int dimStates, double stdProcess, double stdMeasure, double[][] modelProcess)
+            : this(dimStates, dimStates, stdProcess, stdMeasure, modelProcess, Matrix.Build.DenseIdentity(dimStates, dimStates).ToRowArrays())
+        { }
+
+        #region Init
+        public override void Init(double[] estimateGuessed, double[][] covarianceInit)
+        {
+            x_est = Vector.Build.Dense(estimateGuessed);
+            P = Matrix.Build.DenseOfRowArrays(covarianceInit);
+
+            filterStepState = FilterState.Initialized;
+        }
+        #endregion
+
+        #region Predict
+        public override double[] Predict()
+        {
+            if (!(filterStepState == FilterState.JustUpdated || filterStepState == FilterState.Initialized))
+                throw new Exception("Trying to predict without update or initialization");
+
+            x_est_predicted = Fk * x_est;
+            P_predicted = Fk * P * Fk.Transpose() + Q; //prediction from the model
+
+            filterStepState = FilterState.JustPredicted;
+            return x_est_predicted.ToArray();
+        }
+
+        public double[] Predict(double[][] modelProcess)
+        {
+            Fk = Matrix.Build.DenseOfRowArrays(modelProcess);
+            return Predict();
+        }
+
+        public double[] Predict(Matrix<double> modelProcess)
+        {
+            Fk = modelProcess;
+            return Predict();
+        }
+        #endregion
+
+        #region Update
+        public override double[] Update(double[] measure)
+        {
+            if (filterStepState != FilterState.JustPredicted)
+                throw new Exception("Trying to update without a prediction");
+
+            var S = Hk * P_predicted * Hk.Transpose();
+            var K = P_predicted * Hk.Transpose() * S.Inverse(); // Kalman optimal gain
+
+            var Z = Vector.Build.Dense(measure);
+            var deviation = Z - Hk * x_est_predicted;
+
+            x_est = x_est_predicted + K * deviation; //estimation update
+
+            filterStepState = FilterState.JustUpdated;
+            return x_est.ToArray();
+        }
+
+        public double[] Update(double[] measure, double[][] modelObservation)
+        {
+            Hk = Matrix.Build.DenseOfRowArrays(modelObservation);
+            return Update(measure);
+        }
+        #endregion
+    }
+
+}
