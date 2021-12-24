20691668289c0bb03250276120982148b2ca6279
Romain Tirbisch <romain.tirbisch@inetum.com>
2021-12-24 11:50:36 +0100

    Add toolboxes in scrollview of the menuBar. Bug: scrollview layout isn't right.

--- UMI3D-Browser-Desktop/Assets/Project/Scripts/UI_Controllers/Toolbox/Tools_UIController.cs
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/UI_Controllers/Toolbox/Tools_UIController.cs
@@ -27,12 +27,23 @@ namespace DesktopBrowser.UIControllers.Toolbox
         [SerializeField]
         private Sprite icon;
 
-        public static ToolboxItem_E CloneAndSetup(string name, string buttonClassOn, string buttonClassOff, bool isOn, Action buttonClicked)
+        VisualTreeAsset toolboxVisual;
+        MenuBar_UIController menuBar;
+
+        private void Start()
         {
-            throw new System.NotImplementedException();
+            toolboxVisual = GetUIController("toolboxes").VisualTA;
+            menuBar = GetUIController("menuBar") as MenuBar_UIController;
         }
 
         private void Update()
+        {
+            //TestAddToolboxInGlobalView();
+            //TestAddToolWithSprite();
+            TestAddToolboxInMenuBarScrollView();
+        }
+
+        private void TestAddToolboxInGlobalView()
         {
             if (Input.GetKeyDown(KeyCode.T))
             {
@@ -44,7 +55,6 @@ namespace DesktopBrowser.UIControllers.Toolbox
                         Debug.Log("<color=green>TODO: </color>" + $"");
                     }
                 }.SetIcon("menuBar-toolbox", "menuBar-toolbox");
-
                 ToolboxItem_E tool1 = new ToolboxItem_E(VisualTA)
                 {
                     ItemName = "Avatar",
@@ -53,7 +63,6 @@ namespace DesktopBrowser.UIControllers.Toolbox
                         Debug.Log("<color=green>TODO: </color>" + $"");
                     }
                 }.SetIcon("menuBar-avatarOn", "menuBar-avatarOff");
-
                 ToolboxItem_E tool2 = new ToolboxItem_E(VisualTA)
                 {
                     ItemName = "",
@@ -62,7 +71,6 @@ namespace DesktopBrowser.UIControllers.Toolbox
                         Debug.Log("<color=green>TODO: </color>" + $"");
                     }
                 }.SetIcon("toolbox", "toolbox");
-
                 ToolboxItem_E tool3 = new ToolboxItem_E(VisualTA)
                 {
                     ItemName = "",
@@ -71,24 +79,120 @@ namespace DesktopBrowser.UIControllers.Toolbox
                         Debug.Log("<color=green>TODO: </color>" + $"");
                     }
                 }.SetIcon("toolbox", "toolbox");
-
                 Toolbox_E toolbox = new Toolbox_E(GetUIController("toolboxes").VisualTA, tool, tool1, tool2, tool3)
                 {
                     toolboxName = "test"
                 };
-
                 toolbox.AddTo(UIDoc.rootVisualElement);
+            }
+        }
+        private void TestAddToolWithSprite()
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                ToolboxItem_E tool = new ToolboxItem_E(VisualTA)
+                {
+                    ItemName = "Toolbox",
+                    ItemClicked = () =>
+                    {
+                        Debug.Log("<color=green>TODO: </color>" + $"");
+                    }
+                }.SetIcon(icon);
+                Toolbox_E toolbox = new Toolbox_E(GetUIController("toolboxes").VisualTA, tool)
+                {
+                    toolboxName = "test"
+                };
+                toolbox.AddTo(UIDoc.rootVisualElement);
+            }
+        }
+        private void TestAddToolboxInMenuBarScrollView()
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                ToolboxItem_E tool = new ToolboxItem_E(VisualTA)
+                {
+                    ItemName = "Toolbox",
+                    ItemClicked = () =>
+                    {
+                        Debug.Log("<color=green>TODO: </color>" + $"");
+                    }
+                }.SetIcon("menuBar-toolbox", "menuBar-toolbox");
+                ToolboxItem_E tool1 = new ToolboxItem_E(VisualTA)
+                {
+                    ItemName = "Avatar",
+                    ItemClicked = () =>
+                    {
+                        Debug.Log("<color=green>TODO: </color>" + $"");
+                    }
+                }.SetIcon("menuBar-avatarOn", "menuBar-avatarOff");
+                Toolbox_E toolbox = new Toolbox_E(toolboxVisual, tool, tool1)
+                {
+                    toolboxName = "Test"
+                };
 
-                //ToolboxItem_E tool = new ToolboxItem_E(Visual)
-                //{
-                //    ItemName = "Toolbox",
-                //    ItemClicked = () =>
-                //    {
-                //        Debug.Log("<color=green>TODO: </color>" + $"");
-                //    }
-                //}.SetIcon(icon);
+                ToolboxItem_E tool2 = new ToolboxItem_E(VisualTA)
+                {
+                    ItemName = "",
+                    ItemClicked = () =>
+                    {
+                        Debug.Log("<color=green>TODO: </color>" + $"");
+                    }
+                }.SetIcon("menuBar-toolbox", "menuBar-toolbox");
+                ToolboxItem_E tool3 = new ToolboxItem_E(VisualTA)
+                {
+                    ItemName = "Mic",
+                    ItemClicked = () =>
+                    {
+                        Debug.Log("<color=green>TODO: </color>" + $"");
+                    }
+                }.SetIcon("menuBar-micOn", "menuBar-micOff");
+                Toolbox_E toolbox1 = new Toolbox_E(toolboxVisual, tool2, tool3)
+                {
+                    toolboxName = "Test1 avec un titre encore plus plus plus long"
+                };
 
-                //tool.AddTo(controller.UIDoc.rootVisualElement);
+                ToolboxItem_E tool4 = new ToolboxItem_E(VisualTA)
+                {
+                    ItemName = "",
+                    ItemClicked = () =>
+                    {
+                        Debug.Log("<color=green>TODO: </color>" + $"");
+                    }
+                }.SetIcon("menuBar-toolbox", "menuBar-toolbox");
+                ToolboxItem_E tool5 = new ToolboxItem_E(VisualTA)
+                {
+                    ItemName = "Mic",
+                    ItemClicked = () =>
+                    {
+                        Debug.Log("<color=green>TODO: </color>" + $"");
+                    }
+                }.SetIcon("menuBar-micOn", "menuBar-micOff");
+                Toolbox_E toolbox2 = new Toolbox_E(toolboxVisual, tool4, tool5)
+                {
+                    toolboxName = "Test2"
+                };
+
+                ToolboxItem_E tool6 = new ToolboxItem_E(VisualTA)
+                {
+                    ItemName = "",
+                    ItemClicked = () =>
+                    {
+                        Debug.Log("<color=green>TODO: </color>" + $"");
+                    }
+                }.SetIcon("menuBar-toolbox", "menuBar-toolbox");
+                ToolboxItem_E tool7 = new ToolboxItem_E(VisualTA)
+                {
+                    ItemName = "Mic",
+                    ItemClicked = () =>
+                    {
+                        Debug.Log("<color=green>TODO: </color>" + $"");
+                    }
+                }.SetIcon("menuBar-micOn", "menuBar-micOff");
+                Toolbox_E toolbox3 = new Toolbox_E(toolboxVisual, tool6, tool7)
+                {
+                    toolboxName = "Test3"
+                };
+                menuBar.AddToolboxes(toolbox, toolbox1, toolbox2, toolbox3);
             }
         }
     }
