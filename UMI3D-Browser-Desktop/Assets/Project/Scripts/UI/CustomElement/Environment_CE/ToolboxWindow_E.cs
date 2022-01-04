4c1af91433571ef8b1912a37a5894cfbe35d844b
Romain Tirbisch <romain.tirbisch@inetum.com>
2022-01-04 12:06:40 +0100

    Add ToolboxWindow uxml (wip). Add AbstractScrollView (wip).

---
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/UI/CustomElement/Environment_CE/ToolboxWindow_E.cs
@@ -0,0 +1,26 @@
+using BrowserDesktop.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace DesktopBrowser.UI.CustomElement
+{
+    public partial class ToolboxWindow_E
+    {
+        
+    }
+
+    public partial class ToolboxWindow_E
+    {
+        public ToolboxWindow_E(VisualElement root) : base(root) { }
+    }
+
+    public partial class ToolboxWindow_E : AbstractGenericAndCustomElement
+    {
+        public override void OnApplyUserPreferences()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
