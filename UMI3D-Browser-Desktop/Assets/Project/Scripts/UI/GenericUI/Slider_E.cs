ace87284b6b17fb5b7b52c80b636e6cbd5245546
Romain Tirbisch <romain.tirbisch@inetum.com>
2022-02-15 12:22:25 +0100

    Slider_E (wip).

---
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/UI/GenericUI/Slider_E.cs
@@ -0,0 +1,51 @@
+/*
+Copyright 2019 - 2021 Inetum
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+using umi3DBrowser.UICustomStyle;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.Events;
+using BrowserDesktop.preferences;
+
+namespace umi3dDesktopBrowser.uI.viewController
+{
+    public partial class Slider_E
+    {
+        
+    }
+
+    public partial class Slider_E
+    {
+        public void SetDragger()
+        {
+
+        }
+        public void SetDraggerBorder()
+        {
+
+        }
+        public void SetTracker()
+        {
+
+        }
+    }
+
+    public partial class Slider_E : Visual_E
+    {
+        
+    }
+}
