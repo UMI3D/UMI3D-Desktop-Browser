714f8a68427536a425fd2c7455b8824cb5e50005
Romain Tirbisch <romain.tirbisch@inetum.com>
2022-05-13 14:41:25 +0200

    Update namespace. Move dependancies in the new folder.

--- UMI3D-Browser-Desktop/Assets/Dependancies/UMI3DBaseBrowser/UI/GenericUI/Viewl_E/View_E.cs
+++ UMI3D-Browser-Desktop/Assets/Dependancies/UMI3DBaseBrowser/UI/GenericUI/Viewl_E/View_E.cs
@@ -13,9 +13,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-using BrowserDesktop.preferences;
 using System;
 using System.Collections.Generic;
+using umi3d.baseBrowser.preferences;
 using umi3DBrowser.UICustomStyle;
 using UnityEngine;
 using UnityEngine.UIElements;
