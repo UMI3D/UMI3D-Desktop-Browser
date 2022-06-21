448fff21ca869ccbbdd2744e1c7bec9621076e44
Romain Tirbisch <romain.tirbisch@inetum.com>
2022-06-21 17:53:54 +0200

    update notification2d with buttons. Add a box for high priority notification2d.

--- UMI3D-Browser-Desktop/Assets/Project/Scripts/UI/Environment/Notifications/Notification2D_E.cs
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/UI/Environment/Notifications/Notification2D_E.cs
@@ -17,6 +17,7 @@ using System;
 using System.Collections;
 using umi3d.baseBrowser.ui.viewController;
 using umi3d.cdk.menu;
+using umi3DBrowser.UICustomStyle;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -27,7 +28,21 @@ namespace umi3dDesktopBrowser.ui.viewController
         public event Action Complete;
         public Label_E Title { get; protected set; } = null;
         public Label_E Message { get; protected set; } = null;
+        public Button_E ChoiceA { get; protected set; }
+        public Button_E ChoiceB { get; protected set; }
         public ProgressBar_E ProgressBar { get; protected set; } = null;
+        private Action<bool> m_callback;
+
+        protected void ResetButtons()
+        {
+            ChoiceA.ResetClickedEvent();
+            ChoiceB.ResetClickedEvent();
+            VisualManipulator choiceAManip = ChoiceA.GetVisualManipulator(ChoiceA.Root);
+            VisualManipulator choicBAManip = ChoiceB.GetVisualManipulator(ChoiceB.Root);
+
+            choiceAManip.ApplyStyle(MouseBehaviour.MouseOut);
+            choicBAManip.ApplyStyle(MouseBehaviour.MouseOut);
+        }
     }
 
     public partial class Notification2D_E : Box_E
@@ -37,10 +52,26 @@ namespace umi3dDesktopBrowser.ui.viewController
         {
             Title.value = title;
             Message.value = message;
+            ChoiceA.Hide();
+            ChoiceB.Hide();
             if (displayTime > 0)
                 ProgressBar.LaunchTimeBar(displayTime, false);
             else
                 ProgressBar.Hide();
+        }
+
+        public Notification2D_E(string title, string message, string choiceA, string choiceB, Action<bool> callback) :
+            base("UI/UXML/notification2D", "Notification2D", StyleKeys.DefaultBackground)
+        {
+            ResetButtons();
+            Title.value = title;
+            Message.value = message;
+            ChoiceA.Display();
+            ChoiceA.Text = choiceA;
+            ChoiceB.Display();
+            ChoiceB.Text = choiceB;
+            m_callback = callback;
+            ProgressBar.Hide();
         }
 
         protected override void Initialize()
@@ -49,6 +80,10 @@ namespace umi3dDesktopBrowser.ui.viewController
 
             Title = new Label_E(QR<Label>("title"), "TitleNotification2D", StyleKeys.Default_Text_Border);
             Message = new Label_E(QR<Label>("message"), "CorpsNotification2D", StyleKeys.DefaultText);
+            ChoiceA = new Button_E(QR<Button>("choiceA"), "DialogueBoxChoice", StyleKeys.Default);
+            ChoiceB = new Button_E(QR<Button>("choiceB"), "DialogueBoxChoice", StyleKeys.Default);
+            ChoiceA.Clicked += () => m_callback?.Invoke(true);
+            ChoiceB.Clicked += () => m_callback?.Invoke(false);
             ProgressBar = new ProgressBar_E(QR("progressBar"), "ProgressBarNotification2D", null);
             ProgressBar.Complete += () => Complete?.Invoke();
             ProgressBar.SetBar("ProgressBarNotification2D", StyleKeys.DefaultBackground);
