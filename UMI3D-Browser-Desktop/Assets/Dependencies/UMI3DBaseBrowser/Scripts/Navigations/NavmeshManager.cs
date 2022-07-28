12102eeb19f4fa96377e66f6ef150d8739503995
Romain Tirbisch <romain.tirbisch@inetum.com>
2022-07-28 16:47:25 +0200

    Header scripts update

--- UMI3D-Browser-Desktop/Assets/Dependencies/UMI3DBaseBrowser/Scripts/Navigations/NavmeshManager.cs
+++ UMI3D-Browser-Desktop/Assets/Dependencies/UMI3DBaseBrowser/Scripts/Navigations/NavmeshManager.cs
@@ -1,4 +1,9 @@
-﻿/*You may obtain a copy of the License at
+﻿/*
+Copyright 2019 - 2022 Inetum
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
 
@@ -8,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
 using umi3d.cdk;
 using umi3d.common;
 using UnityEngine;
