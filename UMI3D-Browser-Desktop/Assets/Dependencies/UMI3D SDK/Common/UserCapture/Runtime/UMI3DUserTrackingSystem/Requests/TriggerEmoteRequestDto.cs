8ea5cc1f94d246703e79b75b7f01c70046319ddf
Charles-Eole Marichal <91072528+cemarichal-inetum@users.noreply.github.com>
2022-09-05 10:29:19 +0200

    Added Dto ignore by gitignore bug

---
+++ UMI3D-Browser-Desktop/Assets/Dependencies/UMI3D SDK/Common/UserCapture/Runtime/UMI3DUserTrackingSystem/Requests/TriggerEmoteRequestDto.cs
@@ -0,0 +1,34 @@
+﻿//using System.Collections;
+//using UnityEngine;
+
+//namespace umi3d.common.userCapture
+//{
+//    /// <summary>
+//    /// Request from a browser to trigger an emote for its user on user browsers.
+//    /// </summary>
+//    public class TriggerEmoteRequest : AbstractBrowserRequestDto
+//    {
+//        /// <summary>
+//        /// UMI3D id of the emote to trigger.
+//        /// </summary>
+//        public ulong emoteToTriggerId;
+//        /// <summary>
+//        /// User id of the user planning to trigger an emote.
+//        /// </summary>
+//        public ulong sendingUserId;
+
+//        /// <inheritdoc/>
+//        protected override uint GetOperationId()
+//        {
+//            return UMI3DOperationKeys.TriggerEmoteRequest;
+//        }
+
+//        public override Bytable ToBytableArray(params object[] parameters)
+//        {
+//            return base.ToBytableArray(parameters)
+//                + UMI3DNetworkingHelper.Write(emoteToTriggerId)
+//                + UMI3DNetworkingHelper.Write(sendingUserId);
+
+//        }
+//    }
+//}
