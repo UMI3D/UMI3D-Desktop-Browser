2aac48113ee55e8c02539b9c247c3292f7a94d6e
Theo Desbonnets <theo.desbonnets@inetum.com>
2022-09-16 15:47:25 +0200

    update browser version to 2.3.3.220916

--- UMI3D-Browser-Desktop/Assets/Project/Scripts/BrowserVersion.cs
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/BrowserVersion.cs
@@ -27,11 +27,11 @@ namespace BrowserDesktop
         /// <summary>
         /// Build count, to be increament by 1.
         /// </summary>
-        public readonly static string buildCount = "2";
+        public readonly static string buildCount = "3";
         /// <summary>
         /// Year-Month-Day.
         /// </summary>
-        public readonly static string date = "220912";
+        public readonly static string date = "220916";
     }
 }
 
