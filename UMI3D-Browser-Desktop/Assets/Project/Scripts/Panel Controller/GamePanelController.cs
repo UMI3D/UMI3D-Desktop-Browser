473b383f38cc545ab3b26d2eefd3f2935c048f00
Romain Tirbisch <romain.tirbisch@inetum.com>
2022-10-31 10:06:05 +0100

    Update icon position in screen base package.

---
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/Panel Controller/GamePanelController.cs
@@ -0,0 +1,27 @@
+﻿/*
+Copyright 2019 - 2022 Inetum
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+using UnityEngine;
+using UnityEngine.UIElements;
+
+/// <summary>
+/// This class is reponsible for connecting users to environments. It implies asking for login/password or parameters if 
+/// necessary.
+/// </summary>
+public class GamePanelController : umi3d.baseBrowser.connection.BaseGamePanelController
+{
+    public override CustomDialoguebox CreateDialogueBox()
+        => new umi3d.commonScreen.Displayer.Dialoguebox_C();
+}
