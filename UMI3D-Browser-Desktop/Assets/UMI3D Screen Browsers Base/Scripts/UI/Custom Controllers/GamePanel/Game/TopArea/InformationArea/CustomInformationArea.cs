604c90cc8ab50673d15a0bf4884941ed50377912
Romain Tirbisch <romain.tirbisch@inetum.com>
2022-11-15 14:48:37 +0100

    Update player movement

---
+++ UMI3D-Browser-Desktop/Assets/UMI3D Screen Browsers Base/Scripts/UI/Custom Controllers/GamePanel/Game/TopArea/InformationArea/CustomInformationArea.cs
@@ -0,0 +1,438 @@
+/*
+Copyright 2019 - 2022 Inetum
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using umi3d.baseBrowser.ui.viewController;
+using umi3d.common;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CustomInformationArea : VisualElement, ICustomElement
+{
+    public new class UxmlTraits : VisualElement.UxmlTraits
+    {
+        UxmlStringAttributeDescription m_shortText = new UxmlStringAttributeDescription
+        {
+            name = "short-text",
+            defaultValue = null,
+        };
+
+        UxmlBoolAttributeDescription m_isExpanded = new UxmlBoolAttributeDescription
+        {
+            name = "is-expanded",
+            defaultValue = false,
+        };
+
+        UxmlBoolAttributeDescription m_isMicOn = new UxmlBoolAttributeDescription
+        {
+            name = "is-mic-on",
+            defaultValue = false,
+        };
+
+        UxmlBoolAttributeDescription m_isSoundOn = new UxmlBoolAttributeDescription
+        {
+            name = "is-sound-on",
+            defaultValue = true,
+        };
+
+        public override IEnumerable<UxmlChildElementDescription> uxmlChildElementsDescription
+        {
+            get { yield break; }
+        }
+
+        public override void Init(VisualElement ve, IUxmlAttributes bag, CreationContext cc)
+        {
+            base.Init(ve, bag, cc);
+            var custom = ve as CustomInformationArea;
+
+            custom.Set
+                (
+                    m_shortText.GetValueFromBag(bag, cc),
+                    m_isExpanded.GetValueFromBag(bag, cc),
+                    m_isMicOn.GetValueFromBag(bag, cc),
+                    m_isSoundOn.GetValueFromBag(bag, cc)
+                );
+        }
+    }
+
+    public virtual string ShortText
+    {
+        get => ShortInf.text;
+        set 
+        {
+            if (value == ShortInf.text) return;
+            var color = ShortInf.resolvedStyle.color;
+            ShortInf.AddAnimation
+            (
+                this,
+                () => ShortInf.style.color = new Color(color.r, color.g, color.b, color.a),
+                () => ShortInf.style.color = new Color(color.r, color.g, color.b, 0),
+                "color",
+                AnimatorManager.TextFadeDuration,
+                callback: () =>
+                {
+                    ShortInf.text = value;
+                    ShortInf.AddAnimation
+                    (
+                        this,
+                        () => ShortInf.style.color = new Color(color.r, color.g, color.b, 0),
+                        () => ShortInf.style.color = new Color(color.r, color.g, color.b, color.a),
+                        "color",
+                        AnimatorManager.TextFadeDuration,
+                        callback: () => ShortInf.style.color = StyleKeyword.Null
+                    );
+                }
+            );
+        }
+    }
+
+    public virtual bool IsExpanded
+    {
+        get => m_isExplanded;
+        set
+        {
+            if (m_isExplanded == value && !value)
+            {
+                NotificationCenter.ResetNewNotificationFilter();
+                Main.RemoveFromHierarchy();
+                return;
+            }
+            m_isExplanded = value;
+            ExpandUpdate?.Invoke(value);
+            Main.WaitUntil
+            (
+                () => !float.IsNaN(m_shortInfheightLength.value) && !float.IsNaN(m_shortInfWidthgLength.value),
+                () =>
+                {
+                    this.InsertIfNotInHierarchy(0, Main);
+
+                    Main.schedule.Execute(() =>
+                    {
+                        var padingLength = new Length()
+                        {
+                            unit = m_shortInfheightLength.unit,
+                            value = m_shortInfheightLength.value * 1.2f
+                        };
+                        var widthPercent = m_shortInfWidthgLength.unit == LengthUnit.Percent
+                            ? m_shortInfWidthgLength.value
+                            : m_shortInfWidthgLength.value * 100f / this.layout.width;
+
+                        var heightPercent = m_shortInfheightLength.unit == LengthUnit.Percent
+                            ? m_shortInfheightLength.value
+                            : m_shortInfheightLength.value * 100f / this.layout.width;
+
+                        Main.AddAnimation
+                           (
+                               this,
+                               () => Main.style.marginTop = m_shortInfMargin_PaddingLength,
+                               () => Main.style.marginTop = 0f,
+                               "margin-top",
+                               AnimatorManager.MainDuration,
+                               revert: !m_isExplanded
+                           );
+                        Main.AddAnimation
+                        (
+                            this,
+                            () => Main.style.paddingTop = 0f,
+                            () => Main.style.paddingTop = padingLength,
+                            "padding-top",
+                            AnimatorManager.MainDuration,
+                            revert: !m_isExplanded
+                        );
+                        Main.AddAnimation
+                        (
+                            this,
+                            () => Main.style.width = Length.Percent(widthPercent),
+                            () => Main.style.width = Length.Percent(100),
+                            "width",
+                            AnimatorManager.MainDuration,
+                            revert: !m_isExplanded
+                        );
+                        Main.AddAnimation
+                        (
+                            this,
+                            () => Main.style.height = Length.Percent(heightPercent),
+                            () => Main.style.height = Length.Percent(100),
+                            "height",
+                            AnimatorManager.MainDuration,
+                            callback: m_isExplanded ? null : () =>
+                            {
+                                NotificationCenter.ResetNewNotificationFilter();
+                                Main.RemoveFromHierarchy();
+                            },
+                            revert: !m_isExplanded
+                        );
+                    });
+                }
+            );
+        }
+    }
+
+    public virtual bool IsMicOn
+    {
+        get => m_isMicOn;
+        set
+        {
+            m_isMicOn = value;
+            if (value)
+            {
+                Mic.RemoveFromClassList(USSCustomClassMic_Off);
+                Mic.AddToClassList(USSCustomClassMic_On);
+            }
+            else
+            {
+                Mic.RemoveFromClassList(USSCustomClassMic_On);
+                Mic.AddToClassList(USSCustomClassMic_Off);
+            }
+        }
+    }
+
+    public virtual bool IsSoundOn
+    {
+        get => m_isSoundOn;
+        set
+        {
+            m_isSoundOn = value;
+            if (value)
+            {
+                Sound.RemoveFromClassList(USSCustomClassSound_Off);
+                Sound.AddToClassList(USSCustomClassSound_On);
+            }
+            else
+            {
+                Sound.RemoveFromClassList(USSCustomClassSound_On);
+                Sound.AddToClassList(USSCustomClassSound_Off);
+            }
+        }
+    }
+
+    public virtual string StyleSheetGamePath => $"USS/game";
+    public virtual string StyleSheetPath => $"{ElementExtensions.StyleSheetGamesFolderPath}/informationArea";
+    public virtual string USSCustomClassName => "information-area";
+    public virtual string USSCustomClassMain => $"{USSCustomClassName}__main";
+    public virtual string USSCustomClassMain_Background => $"{USSCustomClassName}__main-background";
+    public virtual string USSCustomClassShortInf => $"{USSCustomClassName}__short__inf";
+    public virtual string USSCustomClassMic_Sound => $"{USSCustomClassName}__mic-sound";
+    public virtual string USSCustomClassMic_On => $"{USSCustomClassName}__mic-on";
+    public virtual string USSCustomClassSound_On => $"{USSCustomClassName}__sound-on";
+    public virtual string USSCustomClassMic_Off => $"{USSCustomClassName}__mic-off";
+    public virtual string USSCustomClassSound_Off => $"{USSCustomClassName}__sound-off";
+
+    public event Action<bool> ExpandUpdate;
+    /// <summary>
+    /// True if the mic is on.
+    /// </summary>
+    public event Action MicStatusChanged;
+    /// <summary>
+    /// True if the sound is on.
+    /// </summary>
+    public event Action SoundStatusChanged;
+
+    public event Action NotificationTitleClicked;
+    public string EnvironmentName;
+    public Stack<string> NotificationTitleStack = new Stack<string>();
+    public CustomText ShortInf;
+    public VisualElement Main = new VisualElement { name = "main" };
+    public VisualElement Main_Background = new VisualElement { name = "main-background" };
+    public CustomUserList UserList;
+    public CustomNotificationCenter NotificationCenter;
+    public VisualElement Mic = new VisualElement { name = "mic-icon" };
+    public VisualElement Sound = new VisualElement { name = "sound-icon" };
+    public TouchManipulator2 InfManipulator = new TouchManipulator2(null, 0, 0);
+    public TouchManipulator2 MainManipulator = new TouchManipulator2(null, 0, 0);
+    public TouchManipulator2 ShortInfManipulator = new TouchManipulator2(null, 0, 0);
+    public TouchManipulator2 MicManipulator = new TouchManipulator2(null, 0, 0);
+    public TouchManipulator2 SoundManipulator = new TouchManipulator2(null, 0, 0);
+
+    protected Vector2 m_initialManipulatedPosition;
+    protected bool m_isExplanded;
+    protected bool m_isMicOn;
+    protected bool m_isSoundOn;
+    protected bool m_hasBeenInitialized;
+    protected Length m_shortInfheightLength = float.NaN;
+    protected Length m_shortInfWidthgLength = float.NaN;
+    protected Length m_shortInfMargin_PaddingLength = float.NaN;
+    protected bool m_shortInfExpended = true;
+
+    public virtual void InitElement()
+    {
+        try
+        {
+            this.AddStyleSheetFromPath(StyleSheetGamePath);
+            this.AddStyleSheetFromPath(StyleSheetPath);
+        }
+        catch (System.Exception e)
+        {
+            throw e;
+        }
+        AddToClassList(USSCustomClassName);
+        Main.AddToClassList(USSCustomClassMain);
+        Main_Background.AddToClassList(USSCustomClassMain_Background);
+        ShortInf.AddToClassList(USSCustomClassShortInf);
+        Mic.AddToClassList(USSCustomClassMic_Sound);
+        Sound.AddToClassList(USSCustomClassMic_Sound);
+
+        this.RegisterCallback<CustomStyleResolvedEvent>((evt) =>
+        {
+            this.TryGetCustomStyle("--size__height-short-inf", out m_shortInfheightLength);
+            this.TryGetCustomStyle("--size__width-short-inf", out m_shortInfWidthgLength);
+            this.TryGetCustomStyle("--size-margin-and-padding-game", out m_shortInfMargin_PaddingLength);
+        });
+
+        this.AddManipulator(InfManipulator);
+        InfManipulator.ClickedDownWithInfo += (evt, locaPosition) => m_initialManipulatedPosition = locaPosition;
+        InfManipulator.MovedWithInfo += (evt, localPosition) =>
+        {
+            if (!IsExpanded && m_initialManipulatedPosition.y < localPosition.y) IsExpanded = true;
+        };
+        Main_Background.AddManipulator(MainManipulator);
+        MainManipulator.ClickedDownWithInfo += (evt, locaPosition) => m_initialManipulatedPosition = locaPosition;
+        MainManipulator.MovedWithInfo += (evt, localPosition) =>
+        {
+            if (IsExpanded && m_initialManipulatedPosition.y > localPosition.y) IsExpanded = false;
+        };
+
+        ShortInf.name = "short-inf";
+        ShortInf.AddManipulator(ShortInfManipulator);
+        ShortInfManipulator.LongPressDelay = 400;
+        //ShortInfManipulator.ClickedLong += () => IsExpanded = !IsExpanded;
+        ShortInfManipulator.ClickedDownWithInfo += (e, localPosition) =>
+        {
+            var localToWorld = ShortInf.LocalToWorld(localPosition);
+            if (Mic.ContainsPoint(Mic.WorldToLocal(localToWorld)))
+            {
+                MicManipulator.OnClickedUp();
+                return;
+            }
+            if (Sound.ContainsPoint(Sound.WorldToLocal(localToWorld)))
+            {
+                SoundManipulator.OnClickedUp();
+                return;
+            }
+            if (ShortInf.text != "" && !ShortInf.text.Equals(EnvironmentName))
+            {
+                NotificationTitleClicked?.Invoke();
+                return;
+            }
+
+            //ShortInf.AddAnimation
+            //(
+            //    this,
+            //    () => ShortInf.style.scale = new Scale(Vector3.one),
+            //    () => ShortInf.style.scale = new Scale(new Vector3(1.2f, 1.2f, 1)),
+            //    "scale",
+            //    0.5f,
+            //    forceAnimation: true
+            //);
+        };
+        //ShortInfManipulator.ClickedUp += () =>
+        //{
+        //    ShortInf.AddAnimation
+        //    (
+        //        this,
+        //        () => { },
+        //        () => ShortInf.style.scale = new Scale(Vector3.one),
+        //        "scale",
+        //        0.5f,
+        //        forceAnimation: true
+        //    );
+        //};
+
+        Mic.AddManipulator(MicManipulator);
+        MicManipulator.clicked += () => MicStatusChanged?.Invoke();
+        Sound.AddManipulator(SoundManipulator);
+        SoundManipulator.clicked += () => SoundStatusChanged?.Invoke();
+
+        Main.Add(Main_Background);
+        Main.Add(UserList);
+        Main.Add(NotificationCenter);
+        Add(ShortInf);
+        ShortInf.Add(Mic);
+        ShortInf.Add(Sound);
+    }
+
+    public virtual void Set() => Set(null, false, false, true);
+
+    public virtual void Set(string shortText, bool isExpanded, bool isMicOn, bool isSoundOn)
+    {
+        if (!m_hasBeenInitialized)
+        {
+            InitElement();
+            m_hasBeenInitialized = true;
+        }
+
+        ShortText = shortText;
+        IsExpanded = isExpanded;
+        IsMicOn = isMicOn;
+        IsSoundOn = isSoundOn;
+
+        ShortInf.schedule.Execute(() =>
+        {
+            if (!NotificationTitleStack.TryPeek(out var title))
+            {
+                AnimateShortInf(true);
+                ShortText = null;
+            }
+            else if (!HideNotification)
+            {
+                NotificationTitleStack.Pop();
+                AnimateShortInf(false);
+                var NotifCount = NotificationTitleStack.Count + 1;
+                ShortText = NotifCount == 1 ? $"1 notif: {title}" : $"{NotifCount} notifs: {title}";
+            }
+        }).Every(3000);
+    }
+
+    #region Implementation
+
+    public static bool HideNotification;
+
+    public virtual void AddNotification(NotificationDto dto)
+    {
+        var notification = NotificationCenter.AddNotification(dto);
+        NotificationTitleStack.Push(notification.Title);
+
+        var root = this.FindRoot();
+        root.schedule.Execute(() =>
+        {
+            notification.Timestamp = "0min";
+            root.schedule.Execute(() =>
+            {
+                var time = notification.Timestamp.Substring(0, notification.Timestamp.Length - 3);
+                notification.Timestamp = $"{int.Parse(time) + 1}min";
+            }).Every(60000);
+        }).ExecuteLater(60000);
+    }
+
+    protected void AnimateShortInf(bool isRevert)
+    {
+        if (isRevert != m_shortInfExpended) return;
+        ShortInf.AddAnimation
+        (
+            this,
+            () => ShortInf.style.width = Length.Percent(10),
+            () => ShortInf.style.width = Length.Percent(40),
+            "width",
+            0.5f,
+            delay: isRevert ? 0.5f : 0f,
+            revert: isRevert,
+            callback: () => m_shortInfExpended = !isRevert
+        );
+    }
+
+    #endregion
+}
---
+++ UMI3D-Browser-Desktop/Assets/UMI3D Screen Browsers Base/Scripts/UI/Custom Controllers/GamePanel/Game/TrailingArea/CustomButtonsArea.cs
@@ -0,0 +1,194 @@
+/*
+Copyright 2019 - 2022 Inetum
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CustomButtonsArea : VisualElement, ICustomElement
+{
+    public new class UxmlTraits : VisualElement.UxmlTraits
+    {
+        protected UxmlBoolAttributeDescription m_displayActionButton = new UxmlBoolAttributeDescription
+        {
+            name = "is-action-button-displayed",
+            defaultValue = false
+        };
+        protected UxmlBoolAttributeDescription m_displayEmoteButton = new UxmlBoolAttributeDescription
+        {
+            name = "is-emote-button-displayed",
+            defaultValue = false
+        };
+
+        public override IEnumerable<UxmlChildElementDescription> uxmlChildElementsDescription
+        {
+            get { yield break; }
+        }
+
+        public override void Init(VisualElement ve, IUxmlAttributes bag, CreationContext cc)
+        {
+            base.Init(ve, bag, cc);
+            var custom = ve as CustomButtonsArea;
+
+            custom.Set
+                (
+                    m_displayActionButton.GetValueFromBag(bag, cc),
+                    m_displayEmoteButton.GetValueFromBag(bag, cc)
+                );
+        }
+    }
+
+    public virtual bool IsActionButtonDisplayed
+    {
+        get => m_isActionButtonDisplayed;
+        set
+        {
+            m_isActionButtonDisplayed = value;
+            if (value) Add(Action);
+            else Action.RemoveFromHierarchy();
+        }
+    }
+
+    public virtual bool IsEmoteButtonDisplayed
+    {
+        get => m_isEmoteButtonDisplayed;
+        set
+        {
+            m_isEmoteButtonDisplayed = value;
+            if (value) Add(Emote);
+            else Emote.RemoveFromHierarchy();
+        }
+    }
+
+    public virtual string StyleSheetGamePath => $"USS/game";
+    public virtual string StyleSheetPath => $"{ElementExtensions.StyleSheetGamesFolderPath}/buttonsArea";
+    public virtual string USSCustomClassName => "buttons-area";
+    public virtual string USSCustomClassCrouch => $"{USSCustomClassName}__crouch";
+    public virtual string USSCustomClassjump => $"{USSCustomClassName}__jump";
+    public virtual string USSCustomClassAction => $"{USSCustomClassName}__action";
+    public virtual string USSCustomClassEmote => $"{USSCustomClassName}__emote";
+    public virtual string USSCustomClassCrouchReverse => $"{USSCustomClassName}__crouch_reverse";
+    public virtual string USSCustomClassjumpReverse => $"{USSCustomClassName}__jump_reverse";
+    public virtual string USSCustomClassActionReverse => $"{USSCustomClassName}__action_reverse";
+    public virtual string USSCustomClassEmoteReverse => $"{USSCustomClassName}__emote_reverse";
+
+    public System.Action MainActionDown;
+    public System.Action MainActionUp;
+    public CustomButton Crouch;
+    public CustomButton Jump;
+    public CustomButton Action;
+    public CustomButton Emote;
+
+    public System.Action<EventBase, Vector2> ClickedDown;
+    public System.Action<EventBase, Vector2> Moved;
+
+    public static bool IsLeftHand;
+
+    protected bool m_isActionButtonDisplayed;
+    protected bool m_isEmoteButtonDisplayed;
+    protected bool m_hasBeenInitialized;
+
+    public virtual void InitElement()
+    {
+        try
+        {
+            this.AddStyleSheetFromPath(StyleSheetGamePath);
+            this.AddStyleSheetFromPath(StyleSheetPath);
+        }
+        catch (System.Exception e)
+        {
+            throw e;
+        }
+        AddToClassList(USSCustomClassName);
+        if (IsLeftHand)
+        {
+            Crouch.RemoveFromClassList(USSCustomClassCrouch);
+            Crouch.AddToClassList(USSCustomClassCrouchReverse);
+            Jump.RemoveFromClassList(USSCustomClassjump);
+            Jump.AddToClassList(USSCustomClassjumpReverse);
+            Action.RemoveFromClassList(USSCustomClassAction);
+            Action.AddToClassList(USSCustomClassActionReverse);
+            Emote.RemoveFromClassList(USSCustomClassEmote);
+            Emote.AddToClassList(USSCustomClassEmoteReverse);
+        }
+        else
+        {
+            Crouch.RemoveFromClassList(USSCustomClassCrouchReverse);
+            Crouch.AddToClassList(USSCustomClassCrouch);
+            Jump.RemoveFromClassList(USSCustomClassjumpReverse);
+            Jump.AddToClassList(USSCustomClassjump);
+            Action.RemoveFromClassList(USSCustomClassActionReverse);
+            Action.AddToClassList(USSCustomClassAction);
+            Emote.RemoveFromClassList(USSCustomClassEmoteReverse);
+            Emote.AddToClassList(USSCustomClassEmote);
+        }
+        Action.AddToClassList(USSCustomClassAction);
+        Emote.AddToClassList(USSCustomClassEmote);
+
+        Crouch.name = "crouch";
+        Jump.name = "jump";
+        Action.name = "action";
+        Emote.name = "emote";
+
+        Crouch.text = "Crouch";
+        Jump.text = "Jump";
+        Action.text = "Action";
+        Emote.text = "Emote";
+
+        Crouch.Category = ElementCategory.Game;
+        Jump.Category = ElementCategory.Game;
+        Action.Category = ElementCategory.Game;
+        Emote.Category = ElementCategory.Game;
+
+        Crouch.Size = ElementSize.Custom;
+        Jump.Size = ElementSize.Custom;
+        Action.Size = ElementSize.Custom;
+        Emote.Size = ElementSize.Custom;
+
+        Crouch.Shape = ButtonShape.Round;
+        Jump.Shape = ButtonShape.Round;
+        Action.Shape = ButtonShape.Round;
+        Emote.Shape = ButtonShape.Round;
+
+        Action.ClickedDown += () => MainActionDown?.Invoke();
+        Action.ClickedUp += () => MainActionUp?.Invoke();
+
+        Crouch.ClickedDownWithInfo += (evt, localposition) => ClickedDown?.Invoke(evt, Crouch.LocalToWorld(localposition));
+        Jump.ClickedDownWithInfo += (evt, localposition) => ClickedDown?.Invoke(evt, Jump.LocalToWorld(localposition));
+        Action.ClickedDownWithInfo += (evt, localposition) => ClickedDown?.Invoke(evt, Action.LocalToWorld(localposition));
+
+        Crouch.MovedWithInfo += (evt, localposition) => Moved?.Invoke(evt, Crouch.LocalToWorld(localposition));
+        Jump.MovedWithInfo += (evt, localposition) => Moved?.Invoke(evt, Jump.LocalToWorld(localposition));
+        Action.MovedWithInfo += (evt, localposition) => Moved?.Invoke(evt, Action.LocalToWorld(localposition));
+
+        Add(Crouch);
+        Add(Jump);
+    }
+
+    public virtual void Set() => Set(false, false);
+
+    public virtual void Set(bool isActionButtonDisplayed, bool isEmoteButtonDisplayed)
+    {
+        if (!m_hasBeenInitialized)
+        {
+            InitElement();
+            m_hasBeenInitialized = true;
+        }
+
+        IsActionButtonDisplayed = isActionButtonDisplayed;
+        IsEmoteButtonDisplayed = isEmoteButtonDisplayed;
+    }
+}
