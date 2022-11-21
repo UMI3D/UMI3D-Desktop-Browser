6c01282ecf0e1abfa5e9dfba4f65aed906d9fcad
Quentitin-Tran <quentin.tran@inetum.com>
2022-11-21 10:19:35 +0100

    Test voice reducing

--- UMI3D-Browser-Desktop/Assets/Dependencies/UMI3D SDK/ClientDevelopmentKit/Collaboration/Runtime/AudioVideo/NAudioMicrophone.cs
+++ UMI3D-Browser-Desktop/Assets/Dependencies/UMI3D SDK/ClientDevelopmentKit/Collaboration/Runtime/AudioVideo/NAudioMicrophone.cs
@@ -20,6 +20,7 @@ using NAudio.Wave;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using WebSocketSharp;
 using static umi3d.cdk.collaboration.NoiseReducer;
 
 namespace umi3d.cdk.collaboration
@@ -40,9 +41,9 @@ namespace umi3d.cdk.collaboration
         private int currentMicIndex = -1;
 
         /// <summary>
-        /// Current sample rate used to record audio.
+        /// Current sample rate used to record audio. Value forced to 48000 to be able to use noise reduction.
         /// </summary>
-        private int currentMicSampleRate = -1;
+        private readonly int currentMicSampleRate = 48000;
 
         /// <summary>
         /// Two channels are recorded by default because some microphones only record data in one channel.
@@ -100,16 +101,27 @@ namespace umi3d.cdk.collaboration
 
         #region Noise Reducer
 
-        private bool debugLog = false;
-
+        /// <summary>
+        /// Noise reducer handler.
+        /// </summary>
         private NoiseReducer noiseReducer;
 
         [field: SerializeField]
-        private bool UseNoiseReducer { get; set; } = false;
+        [Tooltip("Use noise reduction ?")]
+        private bool UseNoiseReducer { get; set; } = true;
 
+        /// <summary>
+        /// Model used to perform noise reduction.
+        /// </summary>
         private RnNoiseModel noiseModel = RnNoiseModel.Speech;
 
-        private int attenuationStrenght = 20;
+        /// <summary>
+        /// Strenght of noise reduction.
+        /// </summary>
+        private int attenuationStrenght = 50;
+
+        float[] noiseReductionBuffer = new float[480];
+        float[] tmpBuffer = new float[4800];
 
         #endregion
 #endif
@@ -142,14 +154,22 @@ namespace umi3d.cdk.collaboration
 
             Microphone.GetDeviceCaps(Microphone.devices[MicNumberToUse], out int minFreq, out int maxFreq);
 
-
-            currentMicSampleRate = MumbleClient.GetNearestSupportedSampleRate(maxFreq);
             NumSamplesPerOutgoingPacket = MumbleConstants.NUM_FRAMES_PER_OUTGOING_PACKET * currentMicSampleRate / 100;
 
-            if (currentMicSampleRate != 48000)
-                Debug.LogWarning("Using a possibly unsupported sample rate of " + currentMicSampleRate + " things might get weird");
+            if (maxFreq != currentMicSampleRate)
+            {
+                Debug.Log("Microphone supports only " + maxFreq + " Hz, value forced to 48000 to be able to use noise reduction.");
+            }
 
-            InitializeInternalMic(currentMicSampleRate);
+            if (waveIn == null)
+            {
+                waveIn = new WaveInEvent();
+
+                waveIn.DataAvailable += ProcessAudio;
+            }
+
+            waveIn.WaveFormat = new WaveFormat(currentMicSampleRate, numberOfChannel);
+            waveIn.DeviceNumber = MicNumberToUse;
 
 #if UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX
             noiseReducer?.Destroy();
@@ -166,47 +186,68 @@ namespace umi3d.cdk.collaboration
         }
 
         /// <summary>
-        /// Inits <see cref="waveIn"/>.
+        /// Processess audio and add filters if enabled.
         /// </summary>
         /// <param name="micSampleRate"></param>
-        protected void InitializeInternalMic(int micSampleRate)
+        protected void ProcessAudio(object o, WaveInEventArgs a)
         {
-            if (waveIn == null)
+            if (!shouldSendAudioToServer)
+                return;
+
+            byte[] buffer = a.Buffer;
+
+#if UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX
+
+            if (UseNoiseReducer)
             {
-                waveIn = new WaveInEvent();
+                tmpBuffer = new float[4800];
+                int j = 0;
 
-                waveIn.DataAvailable += (s, a) =>
+                for (int index = 0; index < a.BytesRecorded; index += 2 * numberOfChannel)
                 {
-                    if (!shouldSendAudioToServer)
-                        return;
+                    tmpBuffer[j] = ((short)((buffer[channelChoosen + index + 1] << 8) | buffer[channelChoosen + index])) / 32768f;
+                    j++;
+                }
 
-                    byte[] buffer = a.Buffer;
-
-                    lock (data)
+                lock (data)
+                {
+                    for (int i = 0; i < tmpBuffer.Length; i += 480)
                     {
-                        for (int index = 0; index < a.BytesRecorded; index += 2 * numberOfChannel)
-                        {
-                            short sample = (short)((buffer[channelChoosen + index + 1] << 8) | buffer[channelChoosen + index]);
-
-                            data.Add(sample / 32768f);
-                        }
+                        noiseReductionBuffer = tmpBuffer.SubArray(i, 480);
+                        noiseReducer.ReduceNoiseFloat(noiseReductionBuffer, 0);
+                        data.AddRange(noiseReductionBuffer);
                     }
-
-                    if (!isChannelChoosen)
-                        ChooseChannel(buffer, a.BytesRecorded, micSampleRate);
-
-                };
+                }
+            } 
+            else
+            {
+                ConvertAudioInputToData(a.BytesRecorded, buffer);
             }
+#else
+            ConvertAudioInputToData(a.BytesRecorded, buffer);
+#endif
 
-            waveIn.WaveFormat = new WaveFormat(micSampleRate, numberOfChannel);
-            waveIn.DeviceNumber = MicNumberToUse;
+            if (!isChannelChoosen)
+                ChooseChannel(buffer, a.BytesRecorded);
+        }
+
+        private void ConvertAudioInputToData(int ntBytes, byte[] buffer)
+        {
+            lock (data)
+            {
+                for (int index = 0; index < ntBytes; index += 2 * numberOfChannel)
+                {
+                    short sample = (short)((buffer[channelChoosen + index + 1] << 8) | buffer[channelChoosen + index]);
+                    data.Add(sample / 32768f);
+                }
+            }
         }
 
         /// <summary>
         /// Sets up <see cref="channelChoosen"/>, meaning choose the audio input channel to send to the server.
         /// </summary>
         /// <returns></returns>
-        private void ChooseChannel(byte[] buffer, int nbByte, int sampleRate)
+        private void ChooseChannel(byte[] buffer, int nbByte)
         {
             if (!isChannelChoosen)
             {
@@ -250,12 +291,6 @@ namespace umi3d.cdk.collaboration
                     newData = _mumbleClient.GetAvailablePcmArray();
                     newData.Pcm = data.GetRange(0, NumSamplesPerOutgoingPacket).ToArray();
 
-#if UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX
-                    if (UseNoiseReducer)
-                    {
-                        noiseReducer.ReduceNoiseFloat(newData.Pcm, 0);
-                    }
-#endif
                     if (VoiceSendingType == MicType.Amplitude && !AmplitudeHigherThan(MinAmplitude, newData.Pcm))
                     {
                         sendData = false;
@@ -346,9 +381,6 @@ namespace umi3d.cdk.collaboration
         /// </summary>
         protected override void Update()
         {
-            if (Input.GetKeyDown(KeyCode.P))
-                debugLog = !debugLog;
-
             if (_mumbleClient == null || !_mumbleClient.ConnectionSetupFinished)
                 return;
 
@@ -409,38 +441,7 @@ namespace umi3d.cdk.collaboration
             waveIn?.Dispose();
         }
 
-        #endregion
-
-        /*private void OnGUI()
-        {
-            if (debugLog)
-            {
-                if (GUI.Button(new Rect(10, 10, 200, 50), "Change model " + noiseModel.ToString()))
-                {
-                    noiseModel = noiseModel == RnNoiseModel.None ? 0 : noiseModel += 1;
-                    noiseReducer?.ChangeRnnModel(noiseModel);
-                }
-
-                if (GUI.Button(new Rect(10, 60, 50, 50), "+"))
-                {
-                    attenuationStrenght += 10;
-                    noiseReducer?.SetAttenuation(attenuationStrenght);
-                }
-
-                GUI.Label(new Rect(10 + 50 + 10, 60, 50, 50), attenuationStrenght.ToString());
-
-                if (GUI.Button(new Rect(10 + 50 + 10 + 50 + 10, 60, 50, 50), "-"))
-                {
-                    attenuationStrenght -= 10;
-                    noiseReducer?.SetAttenuation(attenuationStrenght);
-                }
-                
-                if (GUI.Button(new Rect(10, 110, 50, 50), "Use noise reduction " + UseNoiseReducer))
-                {
-                    UseNoiseReducer = !UseNoiseReducer;
-                }
-            }
-        }*/
+        #endregion     
     }
 }
 
