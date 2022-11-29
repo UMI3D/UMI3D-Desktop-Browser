c74ba6143cde2e1b667d3c4b446ec0bc3a66ca1f
Romain Tirbisch <romain.tirbisch@inetum.com>
2022-11-29 18:22:24 +0100

    Browser version

--- UMI3D-Browser-Desktop/Assets/Project/Scripts/BrowserVersion.cs
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/BrowserVersion.cs
@@ -27,11 +27,11 @@ namespace BrowserDesktop
         /// <summary>
         /// Build count, to be increament by 1.
         /// </summary>
-        public readonly static string buildCount = "3";
+        public readonly static string buildCount = "6";
         /// <summary>
         /// Year-Month-Day.
         /// </summary>
-        public readonly static string date = "221125";
+        public readonly static string date = "221129";
     }
 }
 
