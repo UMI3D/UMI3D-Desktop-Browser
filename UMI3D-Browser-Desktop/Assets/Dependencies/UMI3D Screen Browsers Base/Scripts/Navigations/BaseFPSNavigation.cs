2f341a047093c065e8e21e9c7d95d5bac3df7e14
Romain Tirbisch <romain.tirbisch@inetum.com>
2022-12-01 14:04:02 +0100

    Update fps navigation (move common methods in the base package).

--- UMI3D-Browser-Desktop/Assets/Dependencies/UMI3D Screen Browsers Base/Scripts/Navigations/BaseFPSNavigation.cs
+++ UMI3D-Browser-Desktop/Assets/Dependencies/UMI3D Screen Browsers Base/Scripts/Navigations/BaseFPSNavigation.cs
@@ -245,6 +245,51 @@ namespace umi3d.baseBrowser.Navigation
             else Destroy(this.gameObject);
         }
 
+        private void Update()
+        {
+            if (!isActive) return;
+
+            OnUpdate();
+        }
+
+        /// <summary>
+        /// return True if the override method can execute, false otherwithe.
+        /// </summary>
+        /// <returns></returns>
+        protected virtual bool OnUpdate()
+        {
+            if (vehicleFreeHead)
+            {
+                state = State.FreeHead;
+
+                if
+                (
+                    !(Controller.BaseCursor.Movement == Controller.BaseCursor.CursorMovement.Free
+                    || Controller.BaseCursor.Movement == Controller.BaseCursor.CursorMovement.FreeHidden)
+                ) HandleView();
+
+                return false;
+            }
+
+            //TODO I Don't know what to do with that
+            //(currentCapsuleBase, currentCapsuleEnd) = GetCapsuleSphereCenters();
+
+            if
+            (
+                (Controller.BaseCursor.Movement == Controller.BaseCursor.CursorMovement.Free
+                || Controller.BaseCursor.Movement == Controller.BaseCursor.CursorMovement.FreeHidden)
+                && navigation != Navigation.Flying
+            )
+            {
+                float height = transform.position.y;
+                ComputeGravity(false, ref height);
+                transform.Translate(0, height - transform.position.y, 0);
+                return false;
+            }
+
+            return true;
+        }
+
         /// <summary>
         /// Applies gravity to player and makes it jump.
         /// </summary>
@@ -400,6 +445,116 @@ namespace umi3d.baseBrowser.Navigation
             Gizmos.DrawWireSphere(collisionHitPoint, .1f);
         }
 #endif
+
+        #region Movement
+
+        /// <summary>
+        /// Moves player.
+        /// </summary>
+        protected void HandleMovement()
+        {
+            float height = transform.position.y;
+
+            Movement = Vector2.zero;
+
+            if (navigateTo)
+            {
+                var delta = navigationDestination - transform.position;
+                Movement = delta.normalized;
+
+                if (Vector3.Distance(transform.position, navigationDestination) < .5f) navigateTo = false;
+            }
+            else UpdateMovement();
+
+            switch (navigation)
+            {
+                case Navigation.Walking:
+                    Walk(ref Movement, ref height);
+                    break;
+                case Navigation.Flying:
+                    Fly(ref Movement, ref height);
+                    break;
+            }
+
+            Movement *= Time.deltaTime;
+
+            Vector3 pos = transform.rotation * new Vector3(Movement.y, 0, Movement.x);
+            pos = ComputeMovement(pos);
+            pos += transform.position;
+            pos.y = height;
+            transform.position = pos;
+        }
+
+        /// <summary>
+        /// Update <see cref="Movement"/> field.
+        /// </summary>
+        protected abstract void UpdateMovement();
+        
+        /// <summary>
+        /// Computes <paramref name="move"/> vector to perform a walk movement and applies gravity.
+        /// </summary>
+        /// <param name="move"></param>
+        /// <param name="height"></param>
+        protected abstract void Walk(ref Vector2 move, ref float height);
+
+        /// <summary>
+        /// Update speed when flying.
+        /// </summary>
+        /// <param name="Move"></param>
+        /// <param name="height"></param>
+        protected void Fly(ref Vector2 Move, ref float height)
+        {
+            Move.x *= data.flyingSpeed;
+            Move.y *= data.flyingSpeed;
+            height += data.flyingSpeed * ((IsCrouching ? -1 : 0) + (IsJumping ? 1 : 0));
+        }
+
+        #endregion
+
+        #region Camera
+        /// <summary>
+        /// Rotates camera.
+        /// </summary>
+        protected abstract void HandleView();
+
+        /// <summary>
+        /// Common part of the HandleView method.
+        /// </summary>
+        /// <param name="angleView"></param>
+        /// <param name="angularSpeed"></param>
+        protected void BaseHandleView(Vector3 angleView, Vector2 angularSpeed)
+        {
+            Vector3 result = angleView + ((Vector3)angularSpeed).NormalizeAngle();
+            if (changeToDefault)
+            {
+                result = lastAngleView;
+                changeToDefault = false;
+            }
+            Vector3 displayResult;
+
+            result.x = Mathf.Clamp(result.x, data.XAngleRange.x, data.XAngleRange.y);
+            displayResult = result;
+            displayResult.x = Mathf.Clamp(displayResult.x, data.XDisplayAngleRange.x, data.XDisplayAngleRange.y);
+
+            if (state == State.Default)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0, result.y, 0));
+                lastAngleView = result;
+            }
+            else
+            {
+                Vector3 angleNeck = transform.rotation.eulerAngles.NormalizeAngle();
+                float delta = Mathf.DeltaAngle(result.y, angleNeck.y);
+
+                if (delta < data.YAngleRange.x) result.y = -data.YAngleRange.x + angleNeck.y;
+                if (delta > data.YAngleRange.y) result.y = -data.YAngleRange.y + angleNeck.y;
+            }
+            viewpoint.transform.rotation = Quaternion.Euler(result);
+            neckPivot.transform.rotation = Quaternion.Euler(new Vector3(Mathf.Clamp(result.x, -maxNeckAngle, maxNeckAngle), result.y, result.z));
+            head.transform.rotation = Quaternion.Euler(displayResult);
+        }
+        #endregion
+
         #endregion
     }
 }
--- UMI3D-Browser-Desktop/Assets/Project/Scripts/Controller/FpsNavigation.cs
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/Controller/FpsNavigation.cs
@@ -16,117 +16,52 @@ limitations under the License.
 
 using BrowserDesktop.Controller;
 using BrowserDesktop.Menu;
+using umi3d.baseBrowser.connection;
 using UnityEngine;
 
 public class FpsNavigation : umi3d.baseBrowser.Navigation.BaseFPSNavigation
 {
     #region Methods
 
-    private void Update()
+    protected override bool OnUpdate()
     {
-        if (!isActive) return;
+        if (!base.OnUpdate()) return false;
 
-        if (vehicleFreeHead)
+        if (TextInputDisplayerElement.isTyping) return false;
+
+        if (state == State.Default && Input.GetKey(InputLayoutManager.GetInputCode(InputLayoutManager.Input.FreeView))) state = State.FreeHead;
+        else if (state == State.FreeHead && !Input.GetKey(InputLayoutManager.GetInputCode(InputLayoutManager.Input.FreeView)))
         {
-            state = State.FreeHead;
-
-            if (!(umi3d.baseBrowser.Controller.BaseCursor.Movement == umi3d.baseBrowser.Controller.BaseCursor.CursorMovement.Free || umi3d.baseBrowser.Controller.BaseCursor.Movement == umi3d.baseBrowser.Controller.BaseCursor.CursorMovement.FreeHidden))
-                HandleView();
-
-            return;
-        }
-
-        (currentCapsuleBase, currentCapsuleEnd) = GetCapsuleSphereCenters();
-
-        float height = transform.position.y;
-
-        if ((umi3d.baseBrowser.Controller.BaseCursor.Movement == umi3d.baseBrowser.Controller.BaseCursor.CursorMovement.Free || umi3d.baseBrowser.Controller.BaseCursor.Movement == umi3d.baseBrowser.Controller.BaseCursor.CursorMovement.FreeHidden))
-        {
-            if (navigation != Navigation.Flying)
-            {
-                ComputeGravity(false, ref height);
-                transform.Translate(0, height - transform.position.y, 0);
-            }
-
-            return;
-        }
-
-        if (TextInputDisplayerElement.isTyping) return;
-
-        if (state == State.Default && Input.GetKey(InputLayoutManager.GetInputCode(InputLayoutManager.Input.FreeView))) 
-        { 
-            state = State.FreeHead; 
-        }
-        else if (state == State.FreeHead && !Input.GetKey(InputLayoutManager.GetInputCode(InputLayoutManager.Input.FreeView))) 
-        {
-            state = State.Default; 
-            changeToDefault = true; 
+            state = State.Default;
+            changeToDefault = true;
         }
 
         HandleMovement();
-
         HandleView();
+
+        return true;
     }
 
     #region Movement
 
     /// <summary>
-    /// Moves player.
+    /// <inheritdoc/>
     /// </summary>
-    private void HandleMovement()
+    protected override void UpdateMovement()
     {
-        float height = transform.position.y;
-
-        Movement = Vector2.zero;
-
-        if (navigateTo)
-        {
-            var delta = navigationDestination - transform.position;
-            Movement = delta.normalized;
-
-            if (Vector3.Distance(transform.position, navigationDestination) < .5f)
-                navigateTo = false;
-        }
-        else
-        {
-            if (Input.GetKey(InputLayoutManager.GetInputCode(InputLayoutManager.Input.Forward))) { Movement.x += 1; }
-            if (Input.GetKey(InputLayoutManager.GetInputCode(InputLayoutManager.Input.Backward))) { Movement.x -= 1; }
-            if (Input.GetKey(InputLayoutManager.GetInputCode(InputLayoutManager.Input.Right))) { Movement.y += 1; }
-            if (Input.GetKey(InputLayoutManager.GetInputCode(InputLayoutManager.Input.Left))) { Movement.y -= 1; }
-            if (Input.GetKeyDown(InputLayoutManager.GetInputCode(InputLayoutManager.Input.Forward))
-                || Input.GetKeyDown(InputLayoutManager.GetInputCode(InputLayoutManager.Input.Backward))
-                || Input.GetKeyDown(InputLayoutManager.GetInputCode(InputLayoutManager.Input.Right))
-                || Input.GetKeyDown(InputLayoutManager.GetInputCode(InputLayoutManager.Input.Left)))
-            {
-                PlayerMoved.Invoke();
-            }
-        }
-
-        switch (navigation)
-        {
-            case Navigation.Walking:
-                Walk(ref Movement, ref height);
-                break;
-            case Navigation.Flying:
-                Fly(ref Movement, ref height);
-                break;
-        }
-
-        Movement *= Time.deltaTime;
-
-        Vector3 pos = transform.rotation * new Vector3(Movement.y, 0, Movement.x);
-        pos = ComputeMovement(pos);
-        pos += transform.position;
-        pos.y = height;
-
-        transform.position = pos;
+        if (Input.GetKey(InputLayoutManager.GetInputCode(InputLayoutManager.Input.Forward))) Movement.x += 1;
+        if (Input.GetKey(InputLayoutManager.GetInputCode(InputLayoutManager.Input.Backward))) Movement.x -= 1;
+        if (Input.GetKey(InputLayoutManager.GetInputCode(InputLayoutManager.Input.Right))) Movement.y += 1;
+        if (Input.GetKey(InputLayoutManager.GetInputCode(InputLayoutManager.Input.Left))) Movement.y -= 1;
+        if (Movement != Vector2.zero) PlayerMoved.Invoke();
     }
 
     /// <summary>
-    /// Computes <paramref name="move"/> vector to perform a walk movement and applies gravitu.
+    /// <inheritdoc/>
     /// </summary>
     /// <param name="move"></param>
-    private void Walk(ref Vector2 move, ref float height)
+    /// <param name="height"></param>
+    protected override void Walk(ref Vector2 move, ref float height)
     {
         IsCrouching = Input.GetKey(InputLayoutManager.GetInputCode(InputLayoutManager.Input.Squat)) || (IsCrouching && !CanJump());
 
@@ -151,58 +86,23 @@ public class FpsNavigation : umi3d.baseBrowser.Navigation.BaseFPSNavigation
         ComputeGravity(Input.GetKey(InputLayoutManager.GetInputCode(InputLayoutManager.Input.Jump)), ref height);
     }
 
-    private void Fly(ref Vector2 Move, ref float height)
-    {
-        Move.x *= data.flyingSpeed;
-        Move.y *= data.flyingSpeed;
-        height += data.flyingSpeed * 0.01f * ((Input.GetKey(InputLayoutManager.GetInputCode(InputLayoutManager.Input.Squat)) ? -1 : 0) + (Input.GetKey(InputLayoutManager.GetInputCode(InputLayoutManager.Input.Jump)) ? 1 : 0));
-    }
-
     #endregion
 
     /// <summary>
-    /// Rotates camera.
+    /// <inheritdoc/>
     /// </summary>
-    private void HandleView()
+    protected override void HandleView()
     {
         if (state == State.FreeMousse) return;
         Vector3 angleView = viewpoint.rotation.eulerAngles.NormalizeAngle();
 
         Vector2 angularSpeed = new Vector2
-            (
+        (
             -1 * Input.GetAxis("Mouse Y") * data.AngularViewSpeed.x, 
             Input.GetAxis("Mouse X") * data.AngularViewSpeed.y
-            );
-        Vector3 result = angleView + ((Vector3)angularSpeed).NormalizeAngle();
-        if (changeToDefault)
-        {
-            result = lastAngleView;
-            changeToDefault = false;
-        }
-        Vector3 displayResult;
+        );
 
-        if (result.x < data.XAngleRange.x) result.x = data.XAngleRange.x;
-        if (result.x > data.XAngleRange.y) result.x = data.XAngleRange.y;
-        displayResult = result;
-        if (displayResult.x < data.XDisplayAngleRange.x) displayResult.x = data.XDisplayAngleRange.x;
-        if (displayResult.x > data.XDisplayAngleRange.y) displayResult.x = data.XDisplayAngleRange.y;
-
-        if (state == State.Default)
-        {
-            transform.rotation = Quaternion.Euler(new Vector3(0, result.y, 0));
-            lastAngleView = result;
-        }
-        else
-        {
-            Vector3 angleNeck = transform.rotation.eulerAngles.NormalizeAngle();
-            float delta = Mathf.DeltaAngle(result.y, angleNeck.y);
-
-            if (delta < data.YAngleRange.x) result.y = -data.YAngleRange.x + angleNeck.y;
-            if (delta > data.YAngleRange.y) result.y = -data.YAngleRange.y + angleNeck.y;
-        }
-        viewpoint.transform.rotation = Quaternion.Euler(result);
-        neckPivot.transform.rotation = Quaternion.Euler(new Vector3(Mathf.Clamp(result.x, -maxNeckAngle, maxNeckAngle), result.y, result.z));
-        head.transform.rotation = Quaternion.Euler(displayResult);
+        BaseHandleView(angleView, angularSpeed);
     }
 
     #endregion
