8bdc8cdd57e4def63d153d6aa2ca7c237de67ee6
Romain Tirbisch <romain.tirbisch@inetum.com>
2023-01-24 15:46:38 +0100

    Add a base class to form so that we can easyly add ad formSDC for scrollable data collection.

--- UMI3D-Browser-Desktop/Assets/Dependencies/UMI3D Screen Browsers Base/Scripts/UI/Custom Controllers/Containers/CustomForm.cs
+++ UMI3D-Browser-Desktop/Assets/Dependencies/UMI3D Screen Browsers Base/Scripts/UI/Custom Controllers/Containers/CustomForm.cs
@@ -17,146 +17,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public abstract class CustomForm : VisualElement, ICustomElement
+public abstract class CustomForm : CustomAbstractScrollableContainer
 {
-    public new class UxmlTraits : VisualElement.UxmlTraits
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public override ElementCategory Category
     {
-        protected UxmlEnumAttributeDescription<ElementCategory> m_category = new UxmlEnumAttributeDescription<ElementCategory> 
-        { 
-            name = "category", 
-            defaultValue = ElementCategory.Menu 
-        };
-        protected UxmlStringAttributeDescription m_title = new UxmlStringAttributeDescription 
-        { 
-            name = "title", 
-            defaultValue = null 
-        };
-        protected UxmlStringAttributeDescription m_iconPath = new UxmlStringAttributeDescription 
-        { 
-            name = "icon-path", 
-            defaultValue = null 
-        };
-
-        public override IEnumerable<UxmlChildElementDescription> uxmlChildElementsDescription
-        {
-            get { yield break; }
-        }
-
-        public override void Init(VisualElement ve, IUxmlAttributes bag, CreationContext cc)
-        {
-            base.Init(ve, bag, cc);
-            var custom = ve as CustomForm;
-
-            custom.Set
-                (
-                    m_category.GetValueFromBag(bag, cc), 
-                    m_title.GetValueFromBag(bag, cc), 
-                    m_iconPath.GetValueFromBag(bag, cc)
-                 );
-        }
-    }
-
-    public virtual string StyleSheetContainerPath => $"USS/container";
-    public virtual string StyleSheetPath => $"{ElementExtensions.StyleSheetContainersFolderPath}/form";
-    public virtual string USSCustomClassName => "form";
-    public virtual string USSCustomClassCategory(ElementCategory category) => $"{USSCustomClassName}-{category}".ToLower();
-    public virtual string USSCustomClassTitle => $"{USSCustomClassName}__title";
-    public virtual string USSCustomClassIcon => $"{USSCustomClassName}__icon";
-
-    public virtual ElementCategory Category
-    {
-        get => m_category;
+        get => base.Category;
         set
         {
-            RemoveFromClassList(USSCustomClassCategory(m_category));
-            AddToClassList(USSCustomClassCategory(value));
-            m_category = value;
+            base.Category = value;
             VScroll.Category = value;
         }
     }
-    public virtual string Title
-    {
-        get => TitleLabel.text;
-        set
-        {
-            m_isSet = false;
-            if (string.IsNullOrEmpty(value)) TitleLabel.RemoveFromHierarchy();
-            else
-            {
-                Insert(0, TitleLabel);
-                TitleLabel.text = value;
-            }
-            m_isSet = true;
-        }
-    }
-    public virtual string IconPath
-    {
-        get => m_iconPath;
-        set
-        {
-            m_isSet = false;
-            if (string.IsNullOrEmpty(value))
-            {
-                m_iconPath = null;
-                IconVisual.style.backgroundImage = StyleKeyword.Null;
-                IconVisual.RemoveFromHierarchy();
-            }
-            else
-            {
-                m_iconPath = value;
-                Insert(1, IconVisual);
-                IconVisual.style.backgroundImage = new StyleBackground(Resources.Load<Texture2D>(value));
-            }
-            m_isSet = true;
-        }
-    }
-    protected ElementCategory m_category;
-    protected string m_iconPath;
-    protected bool m_hasBeenInitialized;
-    protected bool m_isSet = false;
-
-    public CustomText TitleLabel;
-    public VisualElement IconVisual = new VisualElement { name = "icon" };
+    
     public CustomScrollView VScroll;
 
-    public virtual void Set() => Set(ElementCategory.Menu, null, null);
-    public virtual void Set(ElementCategory category, string title, string iconPath)
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public override void InitElement()
     {
-        m_isSet = false;
-
-        if (!m_hasBeenInitialized)
-        {
-            InitElement();
-            m_hasBeenInitialized = true;
-        }
-
-        Category = category;
-        Title = title;
-        IconPath = iconPath;
-
-        m_isSet = true;
-    }
-    public virtual void InitElement()
-    {
-        try
-        {
-            this.AddStyleSheetFromPath(StyleSheetContainerPath);
-            this.AddStyleSheetFromPath(StyleSheetPath);
-        }
-        catch (System.Exception e)
-        {
-            throw e;
-        }
-        AddToClassList(USSCustomClassName);
-        TitleLabel.AddToClassList(USSCustomClassTitle);
-        IconVisual.AddToClassList(USSCustomClassIcon);
-
-        TitleLabel.name = "title";
+        base.InitElement();
 
         Add(VScroll);
-
-        TitleLabel.TextStyle = TextStyle.LowTitle;
     }
 
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
     public override VisualElement contentContainer => m_isSet && VScroll != null ? VScroll.contentContainer : this;
 }
