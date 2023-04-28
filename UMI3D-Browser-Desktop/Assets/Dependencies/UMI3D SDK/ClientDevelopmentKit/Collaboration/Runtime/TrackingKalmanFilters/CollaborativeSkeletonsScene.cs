06558c29c918246d36a7010bedac94c3ebef2ec7
Nicolas Pacqueriaud <nicolas.pacqueriaud@inetum.com>
2023-04-28 11:23:07 +0200

    Modifying browser scene, creating prefabs for the new skeleton systems

--- UMI3D-Browser-Desktop/Assets/Dependencies/UMI3D SDK/ClientDevelopmentKit/Collaboration/Runtime/TrackingKalmanFilters/CollaborativeSkeletonsScene.cs
+++ UMI3D-Browser-Desktop/Assets/Dependencies/UMI3D SDK/ClientDevelopmentKit/Collaboration/Runtime/TrackingKalmanFilters/CollaborativeSkeletonsScene.cs
@@ -19,17 +19,4 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollaborativeSkeletonsScene : SingleBehaviour<CollaborativeSkeletonsScene>
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+public class CollaborativeSkeletonsScene : SingleBehaviour<CollaborativeSkeletonsScene>{}
