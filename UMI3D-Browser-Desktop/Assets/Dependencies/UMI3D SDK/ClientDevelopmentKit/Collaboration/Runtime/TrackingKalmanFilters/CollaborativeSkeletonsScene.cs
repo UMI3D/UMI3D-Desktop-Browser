6604a2b596735aa4466689db648bc930f97d2a0e
CMarichal <charles-eole.marichal@inetum.com>
2023-05-02 16:36:47 +0200

    Update SDK

--- UMI3D-Browser-Desktop/Assets/Dependencies/UMI3D SDK/ClientDevelopmentKit/Collaboration/Runtime/TrackingKalmanFilters/CollaborativeSkeletonsScene.cs
+++ UMI3D-Browser-Desktop/Assets/Dependencies/UMI3D SDK/ClientDevelopmentKit/Collaboration/Runtime/TrackingKalmanFilters/CollaborativeSkeletonsScene.cs
@@ -19,4 +19,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollaborativeSkeletonsScene : SingleBehaviour<CollaborativeSkeletonsScene>{}
+public class CollaborativeSkeletonsScene : SingleBehaviour<CollaborativeSkeletonsScene>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
