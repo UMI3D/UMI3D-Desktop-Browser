950bd93aee88904ace401ef013a0c81db277959e
Romain Tirbisch <romain.tirbisch@inetum.com>
2023-07-27 09:01:09 +0200

    Update browser version.

--- UMI3D-Browser-Desktop/Assets/Project/Scripts/BrowserVersion.cs
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/BrowserVersion.cs
@@ -27,11 +27,11 @@ namespace BrowserDesktop
         /// <summary>
         /// Build count, to be increament by 1.
         /// </summary>
-        public readonly static string buildCount = "12";
+        public readonly static string buildCount = "13";
         /// <summary>
         /// Year-Month-Day.
         /// </summary>
-        public readonly static string date = "230726";
+        public readonly static string date = "230727";
     }
 }
 
