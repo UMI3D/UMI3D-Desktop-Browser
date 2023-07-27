5d9e1b8ca5efbd7ddd9a669c0689d9f6ce93c6ca
Nolan Guyon <nolan.guyon@inetum.com>
2023-07-27 15:33:42 +0200

    Add PercentageSlider

---
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/UI NEW/PercentageSlider_C.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PercentageSlider_C : SliderInt
+{
+    public new class UxmlFactory : UxmlFactory<PercentageSlider_C, UxmlTraits> { }
+
+    public new class UxmlTraits : Label.UxmlTraits
+    {
+        public override void Init(VisualElement ve, IUxmlAttributes bag, CreationContext cc)
+        {
+            base.Init(ve, bag, cc);
+            var custom = ve as PercentageSlider_C;
+
+        }
+    }
+
+    public override int value { 
+        get => base.value;
+        set
+        {
+            base.value = value;
+            _percentageText.value = value.ToString();
+        }
+    }
+
+    private VisualElement _percentage = new VisualElement();
+
+    public TextField _percentageText = new TextField();
+
+    private Label _percentageLabel = new Label();
+
+    private string _integers = "0123456789";
+
+    #region USS
+    /// <summary>
+    /// Add style and theme style sheets to this element.
+    /// </summary>
+    protected virtual void AttachStyleSheet_Impl()
+    {
+    }
+
+    private void AttachStyleSheet()
+    {
+        try
+        {
+            AttachStyleSheet_Impl();
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogException(e);
+        }
+    }
+
+    /// <summary>
+    /// Add Uss custom classes to this element and its children.
+    /// </summary>
+    protected virtual void AttachUssClass()
+    {
+        RemoveFromClassList("unity-label");
+        _percentage.AddToClassList("percentage");
+    }
+    #endregion
+
+    public PercentageSlider_C()
+    {
+        AttachStyleSheet();
+        AttachUssClass();
+        InitElement();
+    }
+
+    /// <summary>
+    /// Initialize this element.
+    /// </summary>
+    protected virtual void InitElement()
+    {
+        highValue = 100;
+
+        _percentageLabel.text = "%";
+        _percentageText.value = "0";
+        _percentageText.RegisterValueChangedCallback(e =>
+        {
+            var r = "";
+            foreach (var str in e.newValue)
+            {
+                if (_integers.Contains(str))
+                    r += str;
+            }
+            if (r != e.newValue)
+            {
+                _percentageText.value = r;
+                return;
+            }
+
+            if (e.newValue.Count() > 3) 
+            {
+                _percentageText.value = e.newValue.Substring(0, 3);
+                return;
+            }
+
+            var v = Int32.Parse(e.newValue);
+            if (v > 100)
+            {
+                _percentageText.value = "100";
+                return;
+            }
+            if (v < 0)
+            {
+                _percentageText.value = "0";
+                return;
+            }
+
+            value = v;
+        });
+
+        _percentage.Add(_percentageText);
+        _percentage.Add(_percentageLabel);
+
+        Add(_percentage);
+    }
+}
