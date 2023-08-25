725a749fbdfea7d60667fb3d0dc12c73265efc3d
Nolan Guyon <nolan.guyon@inetum.com>
2023-08-25 10:36:46 +0200

    UI(Storage): Create UI

---
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/UI NEW/StorageScreen.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine.UIElements;
+
+public class StorageScreen : BaseScreen
+{
+
+
+    public StorageScreen(VisualElement pElement) : base(pElement)
+    {
+
+    }
+}
