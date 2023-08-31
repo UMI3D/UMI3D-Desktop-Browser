2b9d4ea12a73df3a2833366cecf7bbe53a03dc46
TheoDesbonnets <56262448+TheoDesbonnets@users.noreply.github.com>
2023-08-31 14:09:32 +0200

    WIP : avatar working on pc

--- UMI3D-Browser-Desktop/Assets/Dependencies/UMI3D SDK/ClientDevelopmentKit/UserCapture/Runtime/Skeleton/AbstractSkeleton.cs
+++ UMI3D-Browser-Desktop/Assets/Dependencies/UMI3D SDK/ClientDevelopmentKit/UserCapture/Runtime/Skeleton/AbstractSkeleton.cs
@@ -170,6 +170,7 @@ namespace umi3d.cdk.userCapture
         /// <param name="hierarchy"></param>
         private void RetrieveBonesRotation(UMI3DSkeletonHierarchy hierarchy)
         {
+            UnityEngine.Debug.Log($"<color=orange>Compute for {UserId}</color>");
             // consider all bones we should have according to the hierarchy, and set all values to identity
             foreach (var bone in hierarchy.Relations.Keys)
             {
@@ -181,6 +182,7 @@ namespace umi3d.cdk.userCapture
 
             bonesSetByTrackedSkeleton.Clear();
 
+            List<string> deb = new();
             // for each subskeleton, in descending order (lastest has lowest priority),
             // get the relative orientation of all available bones
             lock (SubskeletonsLock)
@@ -194,6 +196,11 @@ namespace umi3d.cdk.userCapture
                     foreach (var b in bones.Where(c => c.boneType != BoneType.Hips))
                     {
                         // if a bone rotation has already been registered, erase it
+                        if(b.boneType == BoneType.Head)
+                        {
+                            deb.Add(skeleton.GetType().Name);
+                        }
+
                         if (Bones.ContainsKey(b.boneType))
                             Bones[b.boneType].Rotation = b.rotation.Quaternion();
                         else
@@ -216,6 +223,8 @@ namespace umi3d.cdk.userCapture
                         }
                     }
                 }
+
+            deb.Debug();
         }
 
         /// <inheritdoc/>
