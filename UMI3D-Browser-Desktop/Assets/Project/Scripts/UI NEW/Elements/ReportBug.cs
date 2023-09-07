b6fcf9aeecd4339ec63a27c229985ca6dacabba3
Nolan Guyon <nolan.guyon@inetum.com>
2023-09-07 13:28:12 +0200

    feat(ReportBug): MailTo

--- UMI3D-Browser-Desktop/Assets/Project/Scripts/UI NEW/Elements/ReportBug.cs
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/UI NEW/Elements/ReportBug.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -5,6 +6,12 @@ public class ReportBug : MonoBehaviour
 {
     [SerializeField]
     protected UIDocument m_UiDocument;
+
+    private Dropdown_C m_DropdownOften;
+    private TextElement m_Message;
+    private TextElement m_Mail;
+
+    private Mail m_MailSender = new Mail();
 
     private void Start()
     {
@@ -18,6 +25,19 @@ public class ReportBug : MonoBehaviour
         reportBug.Q<Button>("ButtonClose").clicked += () =>
         {
             reportBug.AddToClassList("hidden");
+        };
+
+        m_Mail = m_UiDocument.rootVisualElement.Q<TextElement>("AskMail");
+
+        m_DropdownOften = m_UiDocument.rootVisualElement.Q<Dropdown_C>("HowOftenHappen");
+        m_DropdownOften.choices = new List<string>() { "Once", "Some time", "Always" };
+        m_DropdownOften.value = "Once";
+
+        m_Message = m_UiDocument.rootVisualElement.Q<TextElement>("Message");
+
+        m_UiDocument.rootVisualElement.Q<Button>("ButtonSend").clicked += () =>
+        {
+            m_MailSender.Send("Bug", "Mail : " + m_Mail + "\nBug happen : " + m_DropdownOften.value + "\n" +  m_Message.text);
         };
     }
 }
