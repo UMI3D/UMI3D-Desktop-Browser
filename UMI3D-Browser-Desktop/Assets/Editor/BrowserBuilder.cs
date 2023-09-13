9e4df59f90998b479e9e9c52ab967cea29fe3482
TheoDesbonnets <56262448+TheoDesbonnets@users.noreply.github.com>
2023-09-13 09:37:20 +0200

    Build v3.2.22.230913 Compatible with UMI3D SDK 2.7.b.230911

--- UMI3D-Browser-Desktop/Assets/Editor/BrowserBuilder.cs
+++ UMI3D-Browser-Desktop/Assets/Editor/BrowserBuilder.cs
@@ -199,8 +199,8 @@ public class BrowserBuilder : InitedWindow<BrowserBuilder>
             info.text = text;
 
             info.NewTitle($"Create Installer");
-
-            await Iscc.ExecuteISCC("C:/Program Files (x86)/Inno Setup 6/ISCC.exe", data.data.InstallerFilePath, info.NewLine, info.NewError);
+            info.NewLine($"Skipped : todo manualy");
+            //await Iscc.ExecuteISCC("C:/Program Files (x86)/Inno Setup 6/ISCC.exe", data.data.InstallerFilePath, info.NewLine, info.NewError);
 
             if (comit)
             {
--- UMI3D-Browser-Desktop/Assets/Project/Scripts/BrowserVersion.cs
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/BrowserVersion.cs
@@ -27,11 +27,11 @@ namespace BrowserDesktop
         /// <summary>
         /// Build count, to be increment by 1.
         /// </summary>
-        public readonly static string buildCount = "21";
+        public readonly static string buildCount = "22";
         /// <summary>
         /// Year-Month-Day.
         /// </summary>
-        public readonly static string date = "230912";
+        public readonly static string date = "230913";
     }
 }
 
