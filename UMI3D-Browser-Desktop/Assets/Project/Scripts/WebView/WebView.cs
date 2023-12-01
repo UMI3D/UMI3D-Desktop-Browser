fc428a492c262e4db2014ad6e4b6ab88c86dba72
Quentin-Tran <quentin.tran@inetum.com>
2023-12-01 09:19:39 +0100

    Updaet umi3d sdk for webview + remove logs

--- UMI3D-Browser-Desktop/Assets/Project/Scripts/WebView/WebView.cs
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/WebView/WebView.cs
@@ -1,12 +1,9 @@
 /*
 Copyright 2019 - 2023 Inetum
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,207 +11,134 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-using System;
-using umi3d.baseBrowser.inputs.interactions;
-using umi3d.cdk;
 using umi3d.common;
-using umi3d.common.interaction;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
-namespace BrowserDesktop
+namespace umi3d.cdk
 {
-    public class WebView : AbstractUMI3DWebView, IPointerEnterHandler, IPointerExitHandler
+    /// <summary>
+    /// 3D WebView
+    /// </summary>
+    public abstract class AbstractUMI3DWebView : MonoBehaviour, IEntity
     {
-        public static bool IsWebViewFocused { get; private set; } = false;
-
         #region Fields
 
-        [SerializeField]
-        private RectTransform textureTransform = null;
+        private bool _canInteract = true;
 
-        [SerializeField]
-        public RuntimeWebBrowserBasic browser = null;
+        /// <summary>
+        /// Can user interact with webview.
+        /// </summary>
+        public bool canInteract
+        {
+            get => _canInteract;
+            set
+            {
+                if (_canInteract != value)
+                {
+                    _canInteract = value;
+                    OnCanInteractChanged(value);
+                }
+            }
+        }
 
-        [SerializeField]
-        private RectTransform container = null;
+        private string _url = string.Empty;
 
-        [Header("Bottom bar")]
+        /// <summary>
+        /// Web view url
+        /// </summary>
+        public string url
+        {
+            get => _url;
+            set
+            {
+                if (!canUrlBeForced)
+                    return;
 
-        [SerializeField]
-        private RectTransform bottomBarContainer = null;
+                if (_url != value)
+                {
+                    _url = value;
 
-        [SerializeField]
-        private RectTransform nextRectTransform = null;
+                    if (!string.IsNullOrEmpty(_url))
+                        OnUrlChanged(value);
+                }
+            }
+        }
 
-        [SerializeField]
-        private RectTransform previousRectTransform = null;
+        private Vector2 _size = Vector2.zero;
 
-        [SerializeField]
-        private RectTransform homeRectTransform = null;
+        /// <summary>
+        /// Webview size.
+        /// </summary>
+        public Vector2 size
+        {
+            get => _size;
+            set
+            {
+                if (_size != value)
+                {
+                    _size = value;
+                    OnSizeChanged(value);
+                }
+            }
+        }
 
-        [Header("Top bar")]
-        [SerializeField]
-        private RectTransform topBarContainer = null;
+        private Vector2 _textureSize = Vector2.zero;
 
-        [SerializeField]
-        private RectTransform urlRectTransform = null;
+        /// <summary>
+        /// Webview texture dimension.
+        /// </summary>
+        public Vector2 textureSize
+        {
+            get => _textureSize;
+            set
+            {
+                if (_textureSize != value)
+                {
+                    _textureSize = value;
+                    OnTextureSizeChanged(value);
+                }
+            }
+        }
 
-        [SerializeField]
-        private RectTransform searchRectTransform = null;
+        private bool _canUrlBeForced = true;
 
-        [Space]
-
-        [SerializeField]
-        Canvas canvas = null;
-
-        [SerializeField]
-        private InputField urlText = null;
-
-        private ulong id;
-
-        private string previousUrl;
+        /// <summary>
+        /// Can <see cref="url"/> be forced by the server.
+        /// </summary>
+        public bool canUrlBeForced
+        {
+            get => _canUrlBeForced;
+            set
+            {
+                if (_canUrlBeForced != value)
+                {
+                    _canUrlBeForced = value;
+                }
+            }
+        }
 
         #endregion
 
         #region Methods
 
-        protected virtual void Awake()
+        public virtual void Init(UMI3DWebViewDto dto)
         {
-            canvas.worldCamera = Camera.main;
-            canvas.transform.localRotation = Quaternion.identity;
-            canvas.transform.localPosition = Vector3.zero;
+            canUrlBeForced = dto.canUrlBeForced;
+            url = dto.url;
+            size = dto.size.Struct();
+            textureSize = dto.textureSize.Struct();
+            canInteract = dto.canInteract;
 
-            canvas.GetComponent<CanvasScaler>().dynamicPixelsPerUnit = 3;
-
-            browser.browserClient.OnUrlChanged += (url) =>
-            {
-                if (url == previousUrl)
-                {
-                    return;
-                }
-
-                previousUrl = url;
-
-                urlText.text = url;
-
-                var request = new WebViewUrlChangedRequestDto
-                {
-                    url = url,
-                    webViewId = id
-                };
-
-                UMI3DClientServer.SendRequest(request, true);
-            };
+            OnCanInteractChanged(canInteract);
         }
 
-        public override void Init(UMI3DWebViewDto dto)
-        {
-            base.Init(dto);
+        protected abstract void OnUrlChanged(string url);
 
-            id = dto.id;
-        }
+        protected abstract void OnSizeChanged(Vector2 size);
 
-        protected override void OnCanInteractChanged(bool canInteract)
-        {
-            browser.disableMouseInputs = !canInteract;
-            browser.disableKeyboardInputs = !canInteract;
+        protected abstract void OnTextureSizeChanged(Vector2 size);
 
-            bottomBarContainer.gameObject.SetActive(canInteract);
-            topBarContainer.gameObject.SetActive(canInteract);
-        }
-
-        protected override void OnSizeChanged(Vector2 size)
-        {
-            container.localScale = new Vector3(size.x, size.y, 1);
-
-            Vector3[] corners = new Vector3[4];
-
-            textureTransform.GetWorldCorners(corners);
-
-            bottomBarContainer.position = (corners[0] + corners[3]) / 2f;
-            topBarContainer.position = (corners[1] + corners[2]) / 2f;
-
-            topBarContainer.localScale = new Vector3(topBarContainer.localScale.x,
-                topBarContainer.localScale.y / container.localScale.y, topBarContainer.localScale.z);
-
-            bottomBarContainer.localScale = new Vector3(bottomBarContainer.localScale.x,
-                bottomBarContainer.localScale.y / container.localScale.y, bottomBarContainer.localScale.z);
-
-            urlRectTransform.localScale = new Vector3(urlRectTransform.localScale.x * topBarContainer.localScale.y,
-                urlRectTransform.localScale.y, urlRectTransform.localScale.z);
-
-            searchRectTransform.localScale = new Vector3(searchRectTransform.localScale.x * topBarContainer.localScale.y,
-                searchRectTransform.localScale.y, searchRectTransform.localScale.z);
-
-            nextRectTransform.localScale = new Vector3(nextRectTransform.localScale.x * bottomBarContainer.localScale.y,
-                nextRectTransform.localScale.y, nextRectTransform.localScale.z);
-
-            previousRectTransform.localScale = new Vector3(previousRectTransform.localScale.x * bottomBarContainer.localScale.y,
-                previousRectTransform.localScale.y, previousRectTransform.localScale.z);
-
-            homeRectTransform.localScale = new Vector3(homeRectTransform.localScale.x * bottomBarContainer.localScale.y,
-                homeRectTransform.localScale.y, homeRectTransform.localScale.z);
-        }
-
-        protected override async void OnTextureSizeChanged(Vector2 size)
-        {
-            while (!browser.browserClient.ReadySignalReceived && !browser.browserClient.IsConnected)
-            {
-                await UMI3DAsyncManager.Yield();
-            }
-
-            await UMI3DAsyncManager.Yield();
-
-            try
-            {
-                browser.browserClient.Resize(new VoltstroStudios.UnityWebBrowser.Shared.Resolution((uint)size.x, (uint)size.y));
-            } catch (Exception ex)
-            {
-                Debug.LogError("Impossible to resize WebView.");
-                Debug.LogException(ex);
-            }
-        }
-
-        protected override async void OnUrlChanged(string url)
-        {
-            while (!browser.browserClient.ReadySignalReceived && !browser.browserClient.IsConnected)
-            {
-                await UMI3DAsyncManager.Yield();
-            }
-
-            await UMI3DAsyncManager.Yield();
-
-            try
-            {
-                Debug.Log("URL " + url);
-                browser.browserClient.LoadUrl(url);
-            }
-            catch (Exception ex)
-            {
-                Debug.LogError("Impossible to load url " + url);
-                Debug.LogException(ex);
-
-                await UMI3DAsyncManager.Delay(5000);
-
-                browser.browserClient.LoadUrl(url);
-            }
-        }
-
-        public void OnPointerEnter(PointerEventData eventData)
-        {
-            IsWebViewFocused = true;
-
-            BaseKeyInteraction.IsEditingTextField = true;
-        }
-
-        public void OnPointerExit(PointerEventData eventData)
-        {
-            IsWebViewFocused = false;
-
-            BaseKeyInteraction.IsEditingTextField = false;
-        }
+        protected abstract void OnCanInteractChanged(bool canInteract);
 
         #endregion
     }
