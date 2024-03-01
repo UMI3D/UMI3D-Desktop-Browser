c0d96cd1dff6a79c69aa94b9e80722897a6209ad
Romain T <romain.tirbisch@inetum.com>
2024-03-01 10:39:12 +0100

    Build v3.2.54.240301 Compatible with UMI3D SDK 2.8.b.240206

--- UMI3D-Browser-Desktop/Assets/Project/Scripts/BrowserVersion.cs
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/BrowserVersion.cs
@@ -27,11 +27,11 @@ namespace BrowserDesktop
         /// <summary>
         /// Build count, to be increment by 1.
         /// </summary>
-        public readonly static string buildCount = "53";
+        public readonly static string buildCount = "54";
         /// <summary>
         /// Year-Month-Day.
         /// </summary>
-        public readonly static string date = "240229";
+        public readonly static string date = "240301";
     }
 }
 
