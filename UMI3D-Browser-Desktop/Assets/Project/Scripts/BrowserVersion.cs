f5303e44f600bf0ca7ca912bf0bb95ccd7093b14
Quentin-Tran <quentin.tran@inetum.com>
2024-04-09 17:41:16 +0200

    Build v3.2.63.240409 Compatible with UMI3D SDK 2.8.r.240402

--- UMI3D-Browser-Desktop/Assets/Project/Scripts/BrowserVersion.cs
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/BrowserVersion.cs
@@ -27,7 +27,7 @@ namespace BrowserDesktop
         /// <summary>
         /// Build count, to be increment by 1.
         /// </summary>
-        public readonly static string buildCount = "62";
+        public readonly static string buildCount = "63";
         /// <summary>
         /// Year-Month-Day.
         /// </summary>
