fcbbae30e0383a689ae270511dbc2fb104e486ba
Quentin-Tran <quentin.tran@inetum.com>
2024-06-05 10:09:11 +0200

    Player controller : fix pass through the ground

--- UMI3D-Browser-Desktop/Assets/Dependencies/UMI3D Screen Browsers Base/Scripts/Navigations/UMI3DCollisionManager.cs
+++ UMI3D-Browser-Desktop/Assets/Dependencies/UMI3D Screen Browsers Base/Scripts/Navigations/UMI3DCollisionManager.cs
@@ -74,11 +74,11 @@ public sealed class UMI3DCollisionManager
     /// <summary>
     /// Whether or not the player is below the ground.
     /// </summary>
-    public bool IsBelowGround => feetGroundDelta < 0f;
+    public bool IsBelowGround => feetGroundDelta < data.groundYAxis;
     /// <summary>
     /// Whether or not the player is above the ground.
     /// </summary>
-    public bool IsAboveGround => feetGroundDelta > 0f;
+    public bool IsAboveGround => feetGroundDelta > data.groundYAxis;
 
     /// <summary>
     /// Get the ground altitude.<br/>
