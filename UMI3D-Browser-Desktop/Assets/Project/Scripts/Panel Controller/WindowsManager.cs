25ec8e6368e248cacee4d5cf0d5fa4074c86c40a
Romain T. <romain.tirbisch@inetum.com>
2024-07-16 17:30:34 +0200

    Decouple and improve performance of WindowManager

--- UMI3D-Browser-Desktop/Assets/Project/Scripts/Panel Controller/WindowsManager.cs
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/Panel Controller/WindowsManager.cs
@@ -1,5 +1,5 @@
 ﻿/*
-Copyright 2019 Gfi Informatique
+Copyright 2019 - 2024 Inetum
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,150 +17,384 @@ limitations under the License.
 using inetum.unityUtils;
 using System;
 using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
 
 /// <summary>
 /// This class removes the default Windows title bar and set up a custom one.
 /// </summary>
-public class WindowsManager : MonoBehaviour
+public class WindowsManager
 {
-    #region Fields
+    /// <summary>
+    /// Width of the window when the full screen is deactivated.
+    /// </summary>
+    static int widthWindow = widthMonitor / 2;
+    /// <summary>
+    /// Height of the window when the full screen is deactivated.
+    /// </summary>
+    static int heightWindow = heightMonitor / 2;
 
-    public Action<bool> FullScreenEnabled;
-    public Action DisplayDialogueBoxToQuit;
+    /// <summary>
+    /// Width of the monitor.
+    /// </summary>
+    static int widthMonitor
+    {
+        get
+        {
+            return Screen.resolutions[Screen.resolutions.Length - 1].width;
+        }
+    }
+    /// <summary>
+    /// Height of the monitor.
+    /// </summary>
+    static int heightMonitor
+    {
+        get
+        {
+            return Screen.resolutions[Screen.resolutions.Length - 1].height;
+        }
+    }
 
-    private bool isZoomed = false;
-    private bool isFullScreen = false;
-    private int widthWindow = Screen.width / 2;
-    private int heightWindow = Screen.height / 2;
+    /// <summary>
+    /// Whether this window is zoomed.<br/>
+    /// <br/>
+    /// A zoomed window is like a full screen window but with the top bar.
+    /// </summary>
+    static bool IsWindowZoomed
+    {
+        get
+        {
+            return IsZoomed(window);
+        }
+    }
+    /// <summary>
+    /// The last zoomed state.
+    /// </summary>
+    static bool zoomed = false;
 
-    #region Fiels to make the title bar working like the windows one.
+    /// <summary>
+    /// Whether this application is in full screen.
+    /// </summary>
+    static bool IsWindowInFullScreen
+    {
+        get
+        {
+            return IsFullScreen(Screen.fullScreenMode);
+        }
+    }
+    /// <summary>
+    /// The last full screen state.
+    /// </summary>
+    static FullScreenMode fullScreenMode = FullScreenMode.Windowed;
 
+    #if UNITY_STANDALONE_WIN
+
+    /// <summary>
+    /// External method to minimize or maximize the window.<br/>
+    /// <br/>
+    /// See SW_HIDE, SW_SHOWNORMAL, SW_SHOWMINIMIZED, SW_SHOWMAXIMIZED
+    /// </summary>
+    /// <param name="hwnd"></param>
+    /// <param name="nCmdShow"></param>
+    /// <returns></returns>
     [DllImport("user32.dll")]
-    private static extern bool ShowWindow(IntPtr hwnd, int nCmdShow);
+    static extern bool ShowWindow(IntPtr hwnd, int nCmdShow);
+    /// <summary>
+    /// Hide the window.
+    /// </summary>
+    const int SW_HIDE = 0;
+    /// <summary>
+    /// Focus and display the window in its original size.
+    /// </summary>
+    const int SW_SHOWNORMAL = 1;
+    /// <summary>
+    /// Focus and display the window in a windowed state.
+    /// </summary>
+    const int SW_SHOWMINIMIZED = 2;
+    /// <summary>
+    /// Focus and display the window in a full screen state.
+    /// </summary>
+    const int SW_SHOWMAXIMIZED = 3;
 
+    /// <summary>
+    /// Get the application's window.
+    /// </summary>
+    /// <returns></returns>
     [DllImport("user32.dll")]
-    private static extern IntPtr GetActiveWindow();
+    static extern IntPtr GetActiveWindow();
+    /// <summary>
+    /// The active window.
+    /// </summary>
+    static IntPtr window;
 
+    /// <summary>
+    /// Whether this window is zoomed.<br/>
+    /// <br/>
+    /// A zoomed window is like a full screen window but with the top bar.
+    /// </summary>
+    /// <param name="hWnd"></param>
+    /// <returns></returns>
     [DllImport("user32.dll")]
     static extern bool IsZoomed(IntPtr hWnd);
 
-    IntPtr hWnd;
+    #endif
 
-    #endregion
-
-    #endregion
-
-    #region Methods
-
-    #region LifeCycle monoBehaviour
-
-    void Start()
+    static WindowsManager()
     {
-        hWnd = GetActiveWindow();
-        isZoomed = IsZoomed(hWnd);
-        isFullScreen = Screen.fullScreen;
-        UpdateWindowWhenResize();
+        // Get the active window ptr.
+        window = GetActiveWindow();
 
-        Application.wantsToQuit += WantsToQuit;
-        QuittingManager.ShouldWaitForApplicationToQuit = true;
+        // Minimize the window when 'WindowsManagerNotificationKey.Minimize' is sent.
+        NotificationHub.Default.Subscribe(
+            typeof(WindowsManager).FullName,
+            WindowsManagerNotificationKey.Minimize,
+            null,
+            Minimize
+        );
+
+        // Maximize the window when 'WindowsManagerNotificationKey.Maximize' is sent.
+        NotificationHub.Default.Subscribe(
+            typeof(WindowsManager).FullName,
+            WindowsManagerNotificationKey.Maximize,
+            null,
+            Maximize
+        );
+
+        // Switch full screen mode when 'WindowsManagerNotificationKey.FullScreenModeWillChange' is sent.
+        NotificationHub.Default.Subscribe(
+             typeof(WindowsManager).FullName,
+             WindowsManagerNotificationKey.FullScreenModeWillChange,
+             null,
+             FullScreenWillChange
+         );
+
+        // Set the initial resolution.
+        Screen.SetResolution(
+            widthMonitor / 2,
+            heightMonitor / 2,
+            FullScreenMode.Windowed
+        );
     }
 
-    private void OnDestroy()
+    public static void Update()
     {
-        Application.wantsToQuit -= WantsToQuit;
-    }
-
-    void Update()
-    {
-        CheckForWindowResizement();
-    }
-
-    #endregion
-
-    #region Application Quit
-
-    /// <summary>
-    /// Show dialogue box to quit when ApplicationIsQuitting is not ready to quit.
-    /// </summary>
-    /// <returns>True when ApplicationIsQuitting is ready to quit, else false.</returns>
-    private bool WantsToQuit()
-    {
-        bool wantsToQuit = QuittingManager.ApplicationIsQuitting;
-        if (!wantsToQuit) DisplayDialogueBoxToQuit?.Invoke();
-        return wantsToQuit;
-    }
-
-    #endregion
-
-    #region Window resizement
-
-    public void Minimize() => ShowWindow(hWnd, 2);
-    public void Maximize() => SwitchFullScreen(false);
-
-    /// <summary>
-    /// Check if the widow is being zoomed or unzoomed, in fullscreen or not.
-    /// </summary>
-    private void CheckForWindowResizement()
-    {
-        if ((IsZoomed(hWnd) && !isZoomed) ||
-            (!IsZoomed(hWnd) && isZoomed)) //Check if the window is being resized (zoomed or unzoomed)
+        //Check if the window is being resized thanks to the original window tab bar button.
+        if ((IsWindowZoomed && !zoomed) || (!IsWindowZoomed && zoomed)) 
         {
-            UnityEngine.Debug.Log($"message 2");
-            isZoomed = IsZoomed(hWnd);
-            if (isZoomed) SwitchFullScreen(true);
-        }
-        else //The window has been resized with a shortcut
-        {
-            UnityEngine.Debug.Log($"message 3");
-            if (Screen.fullScreen && !isFullScreen) //Check if the window is being resized without being zoomed
+            zoomed = IsWindowZoomed;
+            if (zoomed)
             {
-                UnityEngine.Debug.Log($"message 4");
-                ShowWindow(hWnd, 3);
                 SwitchFullScreen(true);
             }
-
-            else if (!Screen.fullScreen && isFullScreen)
-            {
-                UnityEngine.Debug.Log($"message 5");
-                SwitchFullScreen(false);
-            }
+        }
+        // Check if the window is being resized (full screen) thanks to the alt + enter shortcut.
+        else if (IsWindowInFullScreen && !IsFullScreen(fullScreenMode))
+        {
+            SwitchFullScreen(true);
+        }
+        // Check if the window is being resized (windowed) thanks to the alt + enter shortcut.
+        else if (!IsWindowInFullScreen && IsFullScreen(fullScreenMode))
+        {
+            SwitchFullScreen(false);
         }
 
-        if (!isFullScreen && !isZoomed)
+
+
+
+
+        if (Input.GetKeyUp(KeyCode.W))
         {
-            UnityEngine.Debug.Log($"message 6");
+            ShowWindow(GetActiveWindow(), SW_SHOWMAXIMIZED);
+        }
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            ShowWindow(GetActiveWindow(), SW_SHOWMINIMIZED);
+        }
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            UnityEngine.Debug.Log($"Résolution : {widthMonitor}, {heightMonitor}");
+            Screen.SetResolution(
+                widthMonitor,
+                heightMonitor,
+                FullScreenMode.FullScreenWindow
+            );
+        }
+        if (Input.GetKeyUp(KeyCode.V))
+        {
+            UnityEngine.Debug.Log($"Résolution : {widthMonitor}, {heightMonitor}");
+            Screen.SetResolution(
+                widthMonitor / 2,
+                heightMonitor / 2,
+                FullScreenMode.FullScreenWindow
+            );
+        }
+        if (Input.GetKeyUp(KeyCode.B))
+        {
+            UnityEngine.Debug.Log($"Résolution : {widthMonitor}, {heightMonitor}");
+            Screen.SetResolution(
+                widthMonitor / 2,
+                heightMonitor / 2,
+                FullScreenMode.Windowed
+            );
+        }
+        if (Input.GetKeyUp(KeyCode.N))
+        {
+            UnityEngine.Debug.Log($"{Screen.fullScreenMode}, {IsWindowZoomed}");
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha1))
+        {
+            SwitchFullScreen(true);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha2))
+        {
+            SwitchFullScreen(false);
+        }
+    }
+
+    /// <summary>
+    /// Whether <paramref name="mode"/> is a full screen mode or not.
+    /// </summary>
+    /// <param name="mode"></param>
+    /// <returns></returns>
+    static bool IsFullScreen(FullScreenMode mode)
+    {
+        switch (mode)
+        {
+            case FullScreenMode.ExclusiveFullScreen:
+            case FullScreenMode.FullScreenWindow:
+            case FullScreenMode.MaximizedWindow:
+                return true;
+            case FullScreenMode.Windowed:
+                return false;
+            default:
+                UnityEngine.Debug.LogError($"Not handle");
+                return false;
+        }
+    }
+
+    /// <summary>
+    /// Return the platform specific full screen mode.
+    /// </summary>
+    /// <param name="fullScreen"></param>
+    /// <returns></returns>
+    static FullScreenMode GetFullScreenMode(bool fullScreen)
+    {
+#if UNITY_STANDALONE_WIN
+        return fullScreen ? FullScreenMode.FullScreenWindow : FullScreenMode.Windowed;
+#elif UNITY_STANDALONE_OSX
+        return fullScreen ? FullScreenMode.MaximizedWindow : FullScreenMode.Windowed;
+#endif
+    }
+
+    /// <summary>
+    /// Switch between full screen and window.
+    /// </summary>
+    /// <param name="fullScreen">Set full screen if true, else window.</param>
+    static void SwitchFullScreen(bool fullScreen)
+    {
+        // If the full screen mode is not going to change then return.
+        if ((fullScreen &&  IsFullScreen(fullScreenMode))
+            || (!fullScreen && !IsFullScreen(fullScreenMode)))
+        {
+            return;
+        }
+
+        fullScreenMode = GetFullScreenMode(fullScreen);
+
+        // If the window is going to be in full screen save the actual window resolution.
+        if (fullScreen && (Screen.width != widthMonitor || Screen.height != heightMonitor))
+        {
             widthWindow = Screen.width;
             heightWindow = Screen.height;
         }
-    }
 
-    /// <summary>
-    /// Show or hide resizement button and set resolution.
-    /// </summary>
-    private void UpdateWindowWhenResize()
-    {
-        if (isZoomed) Screen.SetResolution(Screen.width, Screen.height, FullScreenMode.FullScreenWindow);
-        FullScreenEnabled?.Invoke(isFullScreen);
-    }
-
-    /// <summary>
-    /// Switch between fullscreen and window.
-    /// </summary>
-    /// <param name="fullScreen">Set fullscreen if true, else window.</param>
-    private void SwitchFullScreen(bool fullScreen)
-    {
-        UnityEngine.Debug.Log($"SwitchFullScreen");
+        // Set the resolution and the full screen mode.
         Screen.SetResolution(
-            widthWindow,
-            heightWindow,
-            fullScreen ? FullScreenMode.FullScreenWindow : FullScreenMode.Windowed
+            fullScreen ? widthMonitor : widthWindow,
+            fullScreen ? heightMonitor : heightWindow,
+            fullScreenMode
         );
-        isFullScreen = fullScreen;
-        FullScreenEnabled?.Invoke(fullScreen);
+
+        // Notify observers that the full screen mode has changed.
+        NotificationHub.Default.Notify(
+            typeof(WindowsManager).FullName, 
+            WindowsManagerNotificationKey.FullScreenModeChanged, 
+            new() { 
+                { WindowsManagerNotificationKey.FullScreenModeChangedInfo.Mode, fullScreenMode } 
+            }
+        );
     }
 
-    #endregion
+    /// <summary>
+    /// Hide the window.
+    /// </summary>
+    /// <param name="notification"></param>
+    static void Minimize(Notification notification)
+    {
+#if UNITY_STANDALONE_WIN
+        ShowWindow(window, SW_SHOWMINIMIZED);
+#endif
+    }
 
-    #endregion
+    /// <summary>
+    /// If the window is hidden, maximize (display) the window in its previous state, else do nothing.
+    /// </summary>
+    /// <param name="notification"></param>
+    static void Maximize(Notification notification)
+    {
+#if UNITY_STANDALONE_WIN
+        ShowWindow(window, SW_SHOWMAXIMIZED);
+#endif
+    }
+
+    /// <summary>
+    /// Be notified when the full sceen mode will change.
+    /// </summary>
+    /// <param name="notification"></param>
+    static void FullScreenWillChange(Notification notification)
+    {
+        if (!notification.TryGetInfoT(WindowsManagerNotificationKey.FullScreenModeChangedInfo.Mode, out FullScreenMode mode))
+        {
+            return;
+        }
+
+        SwitchFullScreen(mode == FullScreenMode.FullScreenWindow);
+    }
+}
+
+public static class WindowsManagerNotificationKey
+{
+    /// <summary>
+    /// Hide the window.
+    /// </summary>
+    public const string Hide = "Hide";
+
+    /// <summary>
+    /// Hide the window
+    /// </summary>
+    public const string Minimize = "Minimize";
+
+    /// <summary>
+    ///  If the window is hidden, maximize (display) the window in its previous state, else do nothing.
+    /// </summary>
+    public const string Maximize = "Maximize";
+
+    /// <summary>
+    /// Ask to change the full screen mode.
+    /// </summary>
+    public const string FullScreenModeWillChange = "FullScreenModeWillChange";
+
+    /// <summary>
+    /// The full screen mode has changed.
+    /// </summary>
+    public const string FullScreenModeChanged = "FullScreenModeChanged";
+
+    public class FullScreenModeChangedInfo
+    {
+        /// <summary>
+        /// Mode of the full screen. <see cref="FullScreenMode"/>
+        /// </summary>
+        public const string Mode = "Mode";
+    }
 }
