083c371dd0105d00f1d97321b042cef904bc7fa3
TheoDesbonnets <56262448+TheoDesbonnets@users.noreply.github.com>
2024-09-05 14:25:32 +0200

    Build v3.5.36.240905 Compatible with UMI3D SDK 2.9.b.240826

--- UMI3D-Browser-Desktop/Assets/Project/Scripts/BrowserVersion.cs
+++ UMI3D-Browser-Desktop/Assets/Project/Scripts/BrowserVersion.cs
@@ -27,11 +27,11 @@ namespace BrowserDesktop
         /// <summary>
         /// Build count, to be increment by 1.
         /// </summary>
-        public readonly static string buildCount = "35";
+        public readonly static string buildCount = "36";
         /// <summary>
         /// Year-Month-Day.
         /// </summary>
-        public readonly static string date = "240902";
+        public readonly static string date = "240905";
     }
 }
 
