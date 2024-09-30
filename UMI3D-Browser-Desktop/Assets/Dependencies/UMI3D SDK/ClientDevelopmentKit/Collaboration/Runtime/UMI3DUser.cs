7f6eb13929fe7e1d567a3c5e67e9c661fb0b7717
TheoDesbonnets <56262448+TheoDesbonnets@users.noreply.github.com>
2024-09-30 16:51:30 +0200

    Displaying simple user action

--- UMI3D-Browser-Desktop/Assets/Dependencies/UMI3D SDK/ClientDevelopmentKit/Collaboration/Runtime/UMI3DUser.cs
+++ UMI3D-Browser-Desktop/Assets/Dependencies/UMI3D SDK/ClientDevelopmentKit/Collaboration/Runtime/UMI3DUser.cs
@@ -20,6 +20,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using umi3d.common;
 using umi3d.common.collaboration.dto.signaling;
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace umi3d.cdk.collaboration
@@ -454,6 +455,16 @@ namespace umi3d.cdk.collaboration
     public class UserAction : UserActionDto
     {
         ulong environmentId;
+        Texture2D texture2D;
+
+        public async Task<Texture2D> GetTexture() {
+            if (texture2D != null)
+                return texture2D;
+            if (icon2D == null)
+                return null;
+
+            return await loadResources();
+        }
 
         public UserAction(ulong environmentId, UserActionDto dto)
         {
@@ -464,6 +475,33 @@ namespace umi3d.cdk.collaboration
             this.description = dto.description;
             this.icon3D = dto.icon3D;
             this.icon2D = dto.icon2D;
+        }
+
+        async Task<Texture2D> loadResources()
+        {
+
+            if (this.icon2D != null && this.icon2D.variants.Count > 0)
+            {
+                try
+                {
+                    FileDto fileToLoad = UMI3DEnvironmentLoader.AbstractParameters.ChooseVariant(this.icon2D.variants);
+                    if (fileToLoad == null)
+                        return null;
+
+                    string ext = fileToLoad.extension;
+                    IResourcesLoader loader = UMI3DEnvironmentLoader.AbstractParameters.SelectLoader(ext);
+                    if (loader != null)
+                    {
+                        var o = await UMI3DResourcesManager.LoadFile(UMI3DGlobalID.EnvironmentId, fileToLoad, loader);
+                        return (Texture2D)o;
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+            return null;
         }
 
         public void Call()
